greghendershott/racket-mode

commit b09c919ec65045c68a7d688d70803a466306dca1

Add file linkify, definition, and help features:
- When *racket* buffer output includes text describing a file and
  location, the text is automatically "linkified" -- turned into an
  Emacs button. When clicked, the button opens the file at the
  position. Examples of such text include:
    - Racket error messages
    - rackunit failure location messages
    - prints of #<path> objects
    - output from the `racket-find-definition` command (see below)

- The command `racket-press-last-button` (C-c C-l) clicks the
  last button in the *racket* buffer, with the action taking place in
  the current window. (So you don't have to mouse over and click it.)

- `racket-find-definition` (C-c C-d) tries to find the definition of the
   symbol at point (or with a prefix, C-u C-c C-d, as prompted). If
   found emits the file/location and function signature to the *racket*
   buffer. Then can use `racket-press-last-button` (C-c C-l) to go
   there.

- racket-help (C-c C-h) uses `racket/help` to find the symbol at
  point (or with a prefix, C-u C-c C-h, as prompted). If found, a
  web browser opens.

- Errors are now displayed with context ("stack") information, and each
  file location in the "stack" is a navigable button (in case you want
  to jump to that location). The context is displayed in "reverse" order
  and the immediate error location is last, at the bottom. This is
  easier to parse visually, IMHO. Furthermore, it makes it work well
  with C-c C-l, because the immediate error will be the last button.

racket-mode.el
@@ -50,44 +50,10 @@ http://www.gnu.org/licenses/ for details.")
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Like DrRacket's F5 (Run)
 
-(defun racket-eval (str)
-  (let ((w (selected-window)))
-    (save-buffer) ;sufficient our `run!` (wouldn't be for Racket's `enter!`)
-
-    ;; If racket process already visible in a window use that, else
-    ;; use previous window.
-    (let ((rw (get-buffer-window inferior-racket-buffer-name)))
-      (if rw
-          (select-window rw)
-        (other-window -1)))
-
-    (run-racket)
-    (select-window w)
-
-    (comint-send-string (get-inferior-racket-buffer-process) str)
-
-    (pop-to-buffer inferior-racket-buffer-name t)
-    (select-window w)))
-
 (defun racket-run ()
   "Save and evaluate the buffer in a fresh REPL like DrRacket."
   (interactive)
   (racket-eval (format "(run! \"%s\")\n" (buffer-file-name))))
-
-(defun racket-shell (cmd)
-  (let ((w (selected-window)))
-    (save-buffer)
-    (let ((rw (get-buffer-window "*shell*")))
-      (if rw
-          (select-window rw)
-        (other-window -1)))
-    (message (concat cmd "..."))
-    (shell)
-    (racket-pop-to-buffer-same-window "*shell*")
-    (comint-send-string "*shell*" (concat cmd "\n"))
-    (select-window w)
-    (sit-for 3)
-    (message nil)))
 
 (defun racket-racket ()
   "Do `racket <file>` in *shell* buffer."
@@ -110,6 +76,56 @@ To run <file>'s `test` submodule."
   (racket-shell (concat (expand-file-name "raco" (file-name-directory racket-program))
                         " test -x "
                         (shell-quote-argument (buffer-file-name)))))
+
+(defun racket-find-definition (&optional prefix)
+  "Find definition of symbol at point. (EXPERIMENTAL)
+
+Only works if you've Run the buffer so that its namespace is active."
+  (interactive "P")
+  (let ((sym (if prefix
+                 (read-from-minibuffer "Find definition of: "
+                                       (if (symbol-at-point)
+                                           (symbol-name (symbol-at-point))
+                                         ""))
+               (symbol-at-point))))
+    (if sym
+        (racket-eval (format "(def! %s)\n\n" sym))
+      (message "Nothing to find."))))
+
+(defun racket-help (&optional prefix)
+  "Find something in Racket's help."
+  (interactive "P")
+  (let ((sym (if prefix
+                 (read-from-minibuffer "Racket help: "
+                                       (if (symbol-at-point)
+                                           (symbol-name (symbol-at-point))
+                                         ""))
+               (symbol-at-point))))
+    (if sym
+        (racket-eval (format "(doc! %s)\n\n" sym))
+      (message "Nothing to find."))))
+
+;;----------------------------------------------------------------------------
+
+(defun racket-eval (str)
+  (run-racket)
+  (comint-send-string (get-inferior-racket-buffer-process) str)
+  (inferior-racket-show-and-move-to-end))
+
+(defun racket-shell (cmd)
+  (let ((w (selected-window)))
+    (save-buffer)
+    (let ((rw (get-buffer-window "*shell*")))
+      (if rw
+          (select-window rw)
+        (other-window -1)))
+    (message (concat cmd "..."))
+    (shell)
+    (racket-pop-to-buffer-same-window "*shell*")
+    (comint-send-string "*shell*" (concat cmd "\n"))
+    (select-window w)
+    (sit-for 3)
+    (message nil)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Enter = enter + indent
@@ -1722,13 +1738,25 @@ To run <file>'s `test` submodule."
 	(map (make-sparse-keymap "Racket")))
     (set-keymap-parent smap lisp-mode-shared-map)
     (define-key smap [menu-bar scheme] (cons "Racket" map))
+
+    (define-key map [racket-press-last-button]
+      '("Press Last Button" . racket-press-last-button))
+    (define-key map [racket-find-definition]
+      '("Symbol Definition" . racket-find-definition))
+    (define-key map [racket-help]
+      '("Symbol Help" . racket-help))
+
+    (define-key map [separator-1] '(menu-item "--"))
+
     (define-key map [insert-lambda]
       '("Insert λ" . racket-insert-lambda))
     (define-key map [indent-region]
       '("Indent Region" . indent-region))
     (define-key map [comment-dwim]
       '("Comment" . comment-dwim))
+
     (define-key map [separator-1] '(menu-item "--"))
+
     (define-key map [send-def]
       '("Evaluate Last Definition" . racket-send-definition))
     (define-key map [send-region]
@@ -1736,7 +1764,9 @@ To run <file>'s `test` submodule."
     (put 'racket-send-region 'menu-enable 'mark-active)
     (define-key map [send-sexp]
       '("Evaluate Last S-Expression" . racket-send-last-sexp))
+
     (define-key map [separator-2] '(menu-item "--"))
+
     (define-key map [racket-raco-test]
       '("Run Using `raco test' in *shell* buffer" . racket-raco-test))
     (define-key map [racket-test]
@@ -1763,6 +1793,9 @@ All commands in `lisp-mode-shared-map' are inherited by this map.")
 (define-key racket-mode-map "\M-\C-x"        'racket-send-definition)
 (define-key racket-mode-map "\C-x\C-e"       'racket-send-last-sexp)
 (define-key racket-mode-map "\C-c\C-r"       'racket-send-region)
+(define-key racket-mode-map "\C-c\C-h"       'racket-help)
+(define-key racket-mode-map "\C-c\C-d"       'racket-find-definition)
+(define-key racket-mode-map "\C-c\C-l"       'racket-press-last-button)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Files
@@ -1822,6 +1855,9 @@ All commands in `lisp-mode-shared-map' are inherited by this map.")
   (racket-mode-variables nil)
   (setq mode-line-process '(":%s"))
   (setq comint-input-filter (function racket-input-filter))
+  ;; FIXME: This affects all comint. How to limit to our mode?
+  ;; Meanwhile I'm testing in racket-linkify-files.
+  (add-hook 'comint-output-filter-functions 'racket-linkify-files)
   (setq comint-get-old-input (function racket-get-old-input))
   (racket-mode-variables t))
 
@@ -1854,17 +1890,36 @@ If there is a process already running in `*racket*', switch to that buffer.
 Runs the hook `inferior-racket-mode-hook' \(after the `comint-mode-hook'
 is run)."
   (interactive)
-  (unless (comint-check-proc inferior-racket-buffer-name)
-    (set-buffer (make-comint "racket" racket-program nil racket-sandbox-rkt))
-    (inferior-racket-mode))
-  (setq racket-buffer inferior-racket-buffer-name)
-  (racket-pop-to-buffer-same-window inferior-racket-buffer-name))
+  (let ((w (selected-window)))
+    ;; If racket process already visible in a window use that, else
+    ;; use previous window.
+    (let ((rw (get-buffer-window inferior-racket-buffer-name)))
+      (if rw
+          (select-window rw)
+        (other-window -1)))
+
+    (unless (comint-check-proc inferior-racket-buffer-name)
+      (set-buffer (make-comint "racket" racket-program nil racket-sandbox-rkt))
+      (inferior-racket-mode))
+    (setq racket-buffer inferior-racket-buffer-name)
+
+    (racket-pop-to-buffer-same-window inferior-racket-buffer-name)
+    (select-window w)))
+
+(defun inferior-racket-show-and-move-to-end ()
+  (let ((w (selected-window)))
+    (pop-to-buffer inferior-racket-buffer-name t)
+    (select-window (get-buffer-window inferior-racket-buffer-name))
+    (with-current-buffer inferior-racket-buffer-name
+      (goto-char (point-max)))
+    (select-window w)))
 
 (defun racket-send-region (start end)
   "Send the current region to the inferior Racket process."
   (interactive "r")
   (comint-send-region (get-inferior-racket-buffer-process) start end)
-  (comint-send-string (get-inferior-racket-buffer-process) "\n"))
+  (comint-send-string (get-inferior-racket-buffer-process) "\n")
+  (inferior-racket-show-and-move-to-end))
 
 (defun racket-send-definition ()
   "Send the current definition to the inferior Racket process."
@@ -1873,12 +1928,98 @@ is run)."
    (end-of-defun)
    (let ((end (point)))
      (beginning-of-defun)
-     (racket-send-region (point) end))))
+     (racket-send-region (point) end)
+     (inferior-racket-show-and-move-to-end))))
 
 (defun racket-send-last-sexp ()
   "Send the previous sexp to the inferior Racket process."
   (interactive)
-  (racket-send-region (save-excursion (backward-sexp) (point)) (point)))
+  (racket-send-region (save-excursion (backward-sexp) (point)) (point))
+  (inferior-racket-show-and-move-to-end))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; linkify file:line:col output
+
+(defvar racket--linkify-regexp
+  (concat
+   "\\(?:"
+     ;;        1            2            3
+     "^;?[ ]*\\([^ :]+\\):\\([0-9]+\\):\\([0-9]+\\)" ;errs, defns
+   "\\)"
+   "\\|"
+   "\\(?:"
+     ;;                      4            5            6
+     "^location:   (#<path:\\([^>]+\\)> \\([0-9]+\\) \\([0-9]+\\)" ;rackunit
+   "\\)"
+   "\\|"
+   "\\(?:"
+     ;;            7       8/9 are nil
+     "#<path:\\([^>]+\\)>" ;other path
+   "\\)"))
+
+(defun racket--match-string-no-props (a b c)
+  (or (match-string-no-properties a)
+      (match-string-no-properties b)
+      (match-string-no-properties c)))
+
+(defun racket--linkify-file ()
+  (racket--match-string-no-props 1 4 7))
+
+(defun racket--linkify-line ()
+  (string-to-number (or (racket--match-string-no-props 2 5 8) "1")))
+
+(defun racket--linkify-col ()
+  (string-to-number (or (racket--match-string-no-props 3 6 9) "0")))
+
+;; Extent of the entire `file:line:col` item is from start of `file`
+;; to end of `col`. Or in the case of `file` alone, it's obviously
+;; start/end of `file`.
+(defun racket--linkify-ext-beg ()
+  (or (match-beginning 1) (match-beginning 4) (match-beginning 7)))
+(defun racket--linkify-ext-end ()
+  (or (match-end 3) (match-end 6) (match-end 7)))
+
+(defun racket-linkify-files (str)
+  "Make a text button if the new output includes some
+file:line:col information."
+  (when (string-equal (buffer-name) inferior-racket-buffer-name) ;not comint generally
+    (save-excursion
+      (goto-char (point-max))
+      (while (re-search-backward racket--linkify-regexp
+                                 (- (point) (length str))
+                                 t)
+        (make-text-button (racket--linkify-ext-beg)
+                          (racket--linkify-ext-end)
+                          'file (racket--linkify-file)
+                          'line (racket--linkify-line)
+                          'col  (racket--linkify-col)
+                          'action #'racket-go-to-file-line-col
+                          'follow-link t)))))
+
+(defun racket-go-to-file-line-col (btn)
+  "Action for the button: Open the file and go to position."
+  (let ((file (button-get btn 'file))
+        (line (button-get btn 'line))
+        (col  (button-get btn 'col)))
+    (let* ((b (get-file-buffer file))
+           (w (and b (get-buffer-window b))))
+      (if w
+          (select-window w)
+        (find-file file)))
+    (goto-char (point-min))
+    (forward-line (1- line))
+    (forward-char col)))
+
+(defun racket-press-last-button ()
+  "Press the last button in the *racket* buffer. For example if
+button links to a file and position, then go to that file and
+position."
+  (interactive)
+  (with-current-buffer inferior-racket-buffer-name
+    (save-excursion
+      (goto-char (point-max))
+      (backward-button 1 nil nil)
+      (button-activate (button-at (point))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 

defn.rkt
@@ -1,0 +1,197 @@
+#lang racket/base
+
+(require syntax/modread
+         racket/match
+         racket/function
+         racket/pretty
+         racket/contract)
+
+(provide
+ (contract-out
+  [find-definition (-> string? #:expand? boolean? (or/c #f syntax?))]
+  [find-provision (-> string? (or/c #f syntax?))]
+  [display-definition (-> string? void?)]
+  [display-provision (-> string? void?)]))
+
+;; Return a syntax object (or #f) of the definition of `str`.
+;; For significance of #:expand?, see definition-in-stx.
+(define (find-definition str #:expand? expand?) ;; string? -> (or/c #f syntax? 'kernel)
+  (find-x str definition-in-stx #:expand? expand?))
+
+;; Return a syntax object (or #f) of the provide of `str`.
+(define (find-provision str) ;; string? -> (or/c #f syntax? 'kernel)
+  (find-x str provision-in-stx #:expand? #f))
+
+;; string? (symbol? syntax? -> syntax?) boolean? -> (or/c #f syntax? 'kernel)
+(define (find-x str f #:expand? expand?)
+  (define-values (id where)
+    (source (namespace-symbol->identifier (string->symbol str))))
+  (and id
+       (match where
+         ['kernel 'kernel]
+         [path? (f id (file->syntax where #:expand? expand?))]
+         [#f #f])))
+
+;; Display definition location, and if possible, some of the original
+;; unexpanded definition syntax (to show e.g. function arguments names).
+(define (display-definition str) ;; string? -> void?
+  (define stx-expanded (find-definition str #:expand? #t))
+  (define datum-unexpanded (and stx-expanded
+                                (let ([stx (find-definition str #:expand? #f)])
+                                  (and stx (syntax? stx)
+                                       (syntax->datum stx)))))
+  (match* (stx-expanded datum-unexpanded)
+    [(#f _)                 (printf "; No definition found for ~a\n" str)]
+    [('kernel _)            (printf "; The Racket #%kernel defines ~a\n" str)]
+    [(stx (list x y _ ...)) (printf "\n; ~a: (~a ~a ...\n"
+                                    (source-loc-str stx) x y)]
+    [(stx _)                (printf "\n; ~a defines ~a\n"
+                                    (source-loc-str stx) str)]))
+
+(define (display-provision str) ;; string? -> void?
+  (match (find-provision str)
+    [#f (printf "; No provide found for ~a\n" str)]
+    ['kernel (printf "; The Racket #%kernel provides ~a\n" str)]
+    [stx (printf "\n; ~a provides ~a\n"
+                 (source-loc-str stx)
+                 (commented (pretty-format (syntax->datum stx))))]))
+  
+(define (commented str)
+  (regexp-replace* "\n" str "\n;"))
+
+(define (source-loc-str stx)
+  (format "~a:~a:~a"
+          (syntax-source stx)
+          (syntax-line stx)
+          (syntax-column stx)))
+
+;; Return the source where an identifier binding is defined, as well
+;; as the id used in the source (which is not necessarily the same,
+;; e.g. `(provide (rename-out ...`).
+(define (source id) ;; identifier? -> (values (or/c symbol? #f) (or/c path? #f 'kernel))
+  (match (identifier-binding id)
+    [(list source-mpi source-id
+           nominal-source-mpi nominal-source-id
+           source-phase import-phase nominal-export-phase)
+     (match (resolved-module-path-name (module-path-index-resolve source-mpi))
+       [(? path-string? path)        (values nominal-source-id path)]
+       ['#%kernel                    (values nominal-source-id 'kernel)]
+       [(? symbol? sym)              (values nominal-source-id (sym->path sym))]
+       [(list (? symbol? sym) _ ...) (values nominal-source-id (sym->path sym))]
+       [_ (values #f #f)])]
+    [_ (values #f #f)]))
+
+;; When module soure is 'sym or '(sym sym1 ...) treat it as "sym.rkt"
+;; in the current-load-relative-directory.
+(define (sym->path sym)
+  (build-path (current-load-relative-directory) (format "~a.rkt" sym)))
+
+;; Return a syntax object (or #f) for the contents of `file`.
+(define (file->syntax file #:expand? expand?)
+  (define-values (base _ __) (split-path file))
+  (parameterize ([current-load-relative-directory base]
+                 [current-namespace (make-base-namespace)])
+    (define stx (with-handlers ([exn:fail? (const #f)])
+                  (with-module-reading-parameterization
+                   (thunk
+                    (with-input-from-file file read-syntax/count-lines)))))
+    (if expand?
+        (expand stx) ;; do this while current-load-relative-directory is set
+        stx)))
+
+(define (read-syntax/count-lines)
+  (port-count-lines! (current-input-port))
+  (read-syntax))
+
+;; Given a symbol? and syntax?, return syntax? corresponding to the
+;; definition.
+;;
+;; If `stx` is run through expand we can find things defined via
+;; definer macros.
+;;
+;; If `stx` is not run through expand, we will miss some things,
+;; however the syntax will be closer to what a human expects --
+;; e.g. `(define (f x) x)` instead of `(define-values (f) (lambda (x) x))`.
+(define (definition-in-stx sym stx) ;;symbol? syntax? -> syntax?
+  (define (eq-sym? stx)
+    (eq? sym (syntax-e stx)))
+  (syntax-case* stx
+                (module #%module-begin define-values define-syntaxes
+                        define define/contract
+                        define-syntax struct define-struct)
+                syntax-e=?
+    [(module _ _ (#%module-begin . stxs)) (for/or ([stx (syntax->list #'stxs)])
+                                            (definition-in-stx sym stx))]
+    [(define          (s . _) . _)  (eq-sym? #'s) stx]
+    [(define/contract (s . _) . _)  (eq-sym? #'s) stx]
+    [(define s . _)                 (eq-sym? #'s) stx]
+    [(define-values (ss ...) . _)   (ormap eq-sym? (syntax->list #'(ss ...))) stx]
+    [(define-syntax (s .  _) . _)   (eq-sym? #'s) stx]
+    [(define-syntax s . _)          (eq-sym? #'s) stx]
+    [(define-syntaxes (ss ...) . _) (ormap eq-sym? (syntax->list #'(ss ...))) stx]
+    [(define-struct s . _)          (eq-sym? #'s) stx]
+    [(define-struct (s _) . _)      (eq-sym? #'s) stx]
+    [(struct s . _)                 (eq-sym? #'s) stx]
+    [(struct (s _) . _)             (eq-sym? #'s) stx]
+    [_ #f]))
+
+;; For use with syntax-case*. When we use syntax-case for syntax-e equality.
+(define (syntax-e=? a b)
+  (equal? (syntax-e a) (syntax-e b)))
+
+;; Given a symbol? and syntax?, return syntax? corresponding to the
+;; provision. Note that we do NOT want stx to be run through `expand`
+;; because we want the original contract definitions (if any).
+(define (provision-in-stx sym stx) ;;symbol? syntax? -> syntax?
+  (define (eq-sym? stx)
+    (eq? sym (syntax-e stx)))
+  (syntax-case* stx
+                (module #%module-begin #%provde provide provide/contract)
+                syntax-e=?
+    [(module _ _ (#%module-begin . ss)) (ormap (curry provision-in-stx sym)
+                                               (syntax->list #'ss))]
+    [(provide/contract . stxs)
+     (for/or ([stx (syntax->list #'stxs)])
+       (syntax-case stx ()
+         [(s _) (eq-sym? #'s) stx]
+         [_ #f]))]
+    [(provide . stxs)
+     (for/or ([stx (syntax->list #'stxs)])
+       (syntax-case* stx (contract-out) syntax-e=?
+         [(contract-out . stxs)
+          (for/or ([stx (syntax->list #'stxs)])
+            (syntax-case* stx (rename struct) syntax-e=?
+              [(struct s _ ...)     (eq-sym? #'s) stx]
+              [(struct (s _) _ ...) (eq-sym? #'s) stx]
+              [(rename _ s _)       (eq-sym? #'s) stx]
+              [(s _)                (eq-sym? #'s) stx]
+              [_ #f]))]
+         ;; Only care about contracts
+         ;; [s (eq-sym? #'s) stx]
+         [_ #f]))]
+    [(#%provide . ss)
+     (ormap eq-sym? (syntax->list #'ss))
+     (ormap eq-sym? (syntax->list #'ss))]
+    [_ #f]))
+
+(module+ test
+  (require rackunit)
+  (check-equal?
+   (syntax->datum
+    (provision-in-stx
+     'foo
+     #'(module a b
+         (#%module-begin
+          (define x 1)
+          (provide baz)
+          (provide/contract [foo x] [bar y])))))
+    '(foo x))
+  (require net/url racket/port)
+  (check-true
+   (match (with-output-to-string (thunk (display-definition "get-pure-port")))
+     ;; full path depends on OS and Racket version, so check tail:
+     [(pregexp "^.+?/collects/net/url\\.rkt:\\d+:\\d+ defines `get-pure-port`\n$") #t]
+     [x x])))
+
+(require net/url)
+(require racket/dict)

sandbox.rkt
@@ -15,7 +15,7 @@
 ;; request I did awhile ago that hasn't yet been accepted for XREPL).
 
 (provide (rename-out [run-file run!]))
-(require racket/sandbox racket/match)
+(require racket/sandbox racket/match "defn.rkt")
 
 (define (run-file path-str)
   (display (banner))
@@ -90,31 +90,80 @@
                                    (loop))])
         ((current-read-interaction) (object-name in) in)))))
 
+;; From xrepl: "Makes it easy to use meta-tools without user-namespace
+;; contamination."
+(define (eval-sexpr-for-user form)
+  (eval (namespace-syntax-introduce (datum->syntax #f form))))
+
 (define (read-interaction src in)
   (parameterize ([read-accept-reader #t]
                  [read-accept-lang #f])
     (define stx (read-syntax src in))
-    ;; Check for special run commands
-    (syntax-case stx (run! log!)
-      [(run!)                           ;no module
+    ;; Check for special commands
+    (syntax-case stx (run! log! def! doc!)
+      [(run!)
        (raise (exn:run-new-sandbox #f))]
-      [(run! path)                      ;"module.rkt"
-       (string? (syntax-e #'path))
-       (raise (exn:run-new-sandbox (syntax-e #'path)))]
-      [(run! path)                      ;module
-       (symbol? (syntax-e #'path))
-       (raise (exn:run-new-sandbox (symbol->string (syntax-e #'path))))]
+      [(run! str) ;; "module.rkt"
+       (string? (syntax-e #'str))
+       (raise (exn:run-new-sandbox (syntax-e #'str)))]
+      [(run! sym) ;; module
+       (symbol? (syntax-e #'sym))
+       (raise (exn:run-new-sandbox (symbol->string (syntax-e #'sym))))]
       [(log! specs ...)
        (log-display (syntax->datum #'(specs ...)))]
+      [(def! sym)
+       (symbol? (syntax-e #'sym))
+       (call-in-sandbox-context ;; to use sandbox evaluator's namespace
+        (current-eval)
+        (lambda ()
+          (display-definition (symbol->string (syntax-e #'sym)))))]
+      [(doc! sym)
+       (symbol? (syntax-e #'sym))
+       (eval-sexpr-for-user `(begin
+                               (require racket/help)
+                               (help ,(namespace-syntax-introduce #'sym))))]
       ;; The usual
       [_ stx])))
 
-;; Prefix lines with #\; so they appear in comment font-lock
-(define (-error-display-handler s exn)
-  (let* ([s (regexp-replace* #rx"^\n+|\n+$" s "")]
-         [s (regexp-replace* #rx"\n\n+" s "\n")]
-         [s (regexp-replace* #rx"\n" s "\n;")])
-    (printf "; ~a\n" s)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; This is the reverse of the default error display handler. We show
+;; the context "stack" first, from its bottom to top, then the
+;; immediate error at the bottom. I prefer that: easier to scan
+;; visually from end of buffer. Also, this enables the "click last
+;; button" command to find the button for the immediate error's
+;; source.
+
+(define (-error-display-handler str exn)
+  (define context (continuation-mark-set->context (exn-continuation-marks exn)))
+  (eprintf "~a; ~a\n"
+           (if (exn:fail:user? exn) "" (context->string context))
+           (regexp-replace "\n" str "\n;")))
+
+(define (context->string xs)
+  (for/fold ([s ""])
+            ([x (in-list (reverse xs))])
+    (match-define (cons id src) x)
+    (string-append s
+                   (if (or src id) "; " "")
+                   (if src
+                       (let ([source (srcloc-source src)]
+                             [line (srcloc-line src)]
+                             [col  (srcloc-column src)])
+                         (if (and line col)
+                             (format "~a:~a:~a"
+                                     (if (path? source)
+                                         (path->string source)
+                                         source)
+                                     line
+                                     col)
+                             (format "~a" source)))
+                       "")
+                   (if (and src id) " " "")
+                   (if id
+                       (format "~a" id)
+                       "")
+                   (if (or src id) "\n" ""))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 

commit 8880ff8191bdd807fdbe33bff6671e4e15371d1e

racket-run should save-buffer.
Commit b09c919 broke this.

racket-mode.el
@@ -53,6 +53,7 @@ http://www.gnu.org/licenses/ for details.")
 (defun racket-run ()
   "Save and evaluate the buffer in a fresh REPL like DrRacket."
   (interactive)
+  (save-buffer)
   (racket-eval (format "(run! \"%s\")\n" (buffer-file-name))))
 
 (defun racket-racket ()