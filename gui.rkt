e2dfa6de176688267daf797dd7a2a22f93806bed
Greg Hendershott <greghendershott@gmail.com>
2014-04-12 10:20:42 -0400

    Split among files.

---
+++ gui.rkt
@@ -0,0 +1,33 @@
+#lang racket/base
+
+(require "error.rkt")
+
+(provide gui-required?
+         require-gui
+         txt/gui)
+
+(define root-eventspace #f) ;#f until racket/gui/base required first time
+
+(define (gui-required?)
+  (not (not root-eventspace)))
+
+;; This must be called from the main thread, under the main custodian!
+(define (require-gui)
+  (when (gui-required?)
+    (error 'reuire-gui "Already required"))
+  (display-commented "on-demand one-time instantiation of racket/gui/base")
+  (define current-eventspace (gui-dyn-req 'current-eventspace))
+  (define make-eventspace    (gui-dyn-req 'make-eventspace))
+  (set! root-eventspace (make-eventspace))
+  (current-eventspace root-eventspace))
+
+;; Like mz/mr from racket/sandbox.
+(define-syntax txt/gui
+  (syntax-rules ()
+    [(_ txtval guisym)
+     (if (gui-required?)
+         (gui-dyn-req 'guisym)
+         txtval)]))
+
+(define (gui-dyn-req sym)
+  (dynamic-require 'racket/gui/base sym))
--- sandbox.rkt
+++ sandbox.rkt
@@ -2,13 +2,13 @@
 
 (require (for-syntax syntax/parse)
          racket/match
-         racket/format
          racket/string
-         racket/list
          racket/pretty
-         racket/runtime-path
-         syntax/srcloc
-         "defn.rkt")
+         racket/format
+         "defn.rkt"
+         "error.rkt"
+         "gui.rkt"
+         "logger.rkt")
 
 (module+ main
   (display (banner))
@@ -50,34 +50,10 @@
   (newline)
   (match msg
     [(rerun p)  (run p)]
-    [(load-gui) (require-racket/gui/base) (run path-str)]))
+    [(load-gui) (require-gui) (run path-str)]))
 
 (struct rerun (path)) ;(or/c #f path-string?)
 (struct load-gui ())
-
-(define root-eventspace #f) ;#f until racket/gui/base required first time
-
-(define (repl-gui-available?)
-  (not (not root-eventspace)))
-
-;; This must be called from the main thread, under the main custodian!
-(define (require-racket/gui/base)
-  (unless root-eventspace
-    (display-commented "on-demand one-time instantiation of racket/gui/base")
-    (define current-eventspace (dynamic-require 'racket/gui/base
-                                                'current-eventspace))
-    (define make-eventspace    (dynamic-require 'racket/gui/base
-                                                'make-eventspace))
-    (set! root-eventspace (make-eventspace))
-    (current-eventspace root-eventspace)))
-
-;; Like mz/mr from racket/sandbox.
-(define-syntax txt/gui
-  (syntax-rules ()
-    [(_ txtval guisym)
-     (if (repl-gui-available?)
-         (dynamic-require 'racket/gui/base 'guisym)
-         txtval)]))
 
 (define repl-module-name-resolver
   (let ([orig-resolver (current-module-name-resolver)])
@@ -85,8 +61,8 @@
       [(rmp ns)
        (orig-resolver rmp ns)]
       [(mp rmp stx load?)
-       (unless root-eventspace
-         (when (and (eq? mp 'racket/gui/base) load?)
+       (when (and (eq? mp 'racket/gui/base) load?)
+         (unless (gui-required?)
            (raise (load-gui))))
        (orig-resolver mp rmp stx load?)])))
 
@@ -140,152 +116,6 @@
              [(cd) (cd (~a (read)))]
              [else stx])]
           [_ stx])))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define (our-error-display-handler str exn)
-  (when (exn? exn)
-    (unless (equal? "Check failure" (exn-message exn)) ;rackunit check fails
-      (display-commented str)
-      (display-srclocs exn)
-      (unless (exn:fail:user? exn)
-        (display-context exn)))))
-
-(define (display-srclocs exn)
-  (when (exn:srclocs? exn)
-    (let* ([srclocs ((exn:srclocs-accessor exn) exn)]
-           ;; Some exns' first srcloc is already in exn-message; skip it
-           [srclocs (cond [(or (exn:fail:read? exn)
-                               (exn:fail:syntax? exn)
-                               (exn:fail:contract:variable? exn))
-                           (cdr srclocs)]
-                          [else srclocs])])
-      (for ([srcloc srclocs])
-        (display-commented (source-location->string srcloc))))))
-
-(define (display-context exn)
-  (define ctx (continuation-mark-set->context (exn-continuation-marks exn)))
-  (match (context->string ctx)
-    ["" (void)]
-    [s (display-commented "Context:")
-       (display-commented s)]))
-
-(define (context->string xs)
-  (string-join (for/list ([x xs]
-                          [n 10]
-                          #:break (system-context? x))
-                 (context-item->string x))
-               "\n"))
-
-(define-runtime-path sandbox.rkt ".")
-(define (system-context? ci)
-  (match-define (cons id src) ci)
-  (or (not src)
-      (let ([src (srcloc-source src)])
-        (and (path? src)
-             (or (equal? src sandbox.rkt)
-                 (under-system-path? src))))))
-
-(define (under-system-path? path)
-  (define excluded-collections
-    '("typed/racket" "racket/sandbox" "racket/contract" "racket/private"))
-  (define-values (dir base _) (split-path path))
-  (not (not (for/or ([collection (in-list excluded-collections)])
-              (collection-file-path base collection #:fail (lambda _ #f))))))
-
-(module+ test
-  (require rackunit)
-  (check-true (under-system-path?
-   (string->path "/Applications/Racket_v5.93/share/pkgs/typed-racket-lib/typed-racket/tc-setup.rkt"))))
-
-(define (context-item->string ci)
-  (match-define (cons id src) ci)
-  (string-append (if (or src id) " " "")
-                 (if src (source-location->string src) "")
-                 (if (and src id) " " "")
-                 (if id (format "~a" id) "")))
-
-(define (display-exn exn)
-  (our-error-display-handler (exn-message exn) exn))
-
-(define (display-commented str)
-  (eprintf "; ~a\n"
-           (regexp-replace* "\n" str "\n; ")))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define current-log-receiver-thread (make-parameter #f))
-(define global-logger (current-logger))
-(define other-level 'fatal)
-
-;; Default a couple specific loggers one notch above their "noisy"
-;; level. That way, if someone sets "all other" loggers to e.g. debug,
-;; these won't get noisy. They need to be specifically cranked up.
-(define logger-levels (make-hasheq '([cm-accomplice . warning]
-                                     [gc . info])))
-
-(define racket-log-file (build-path (find-system-path 'temp-dir) "racket-log"))
-(with-output-to-file racket-log-file #:exists 'truncate void)
-
-(define (update-log-receiver)
-  (show-logger-levels) ;handy to show after setting
-  (cond [(current-log-receiver-thread) => kill-thread])
-  (let* ([args (append (list global-logger)
-                       (flatten (for/list ([(k v) logger-levels])
-                                  (list v k)))
-                       (list other-level))]
-         [r (apply make-log-receiver args)])
-    (current-log-receiver-thread
-     (thread
-      (λ ()
-         (let loop ()
-           (match (sync r)
-             [(vector l m v name)
-              ;; To stderr
-              (eprintf "; [~a] ~a\n" l m)
-              (flush-output)
-              ;; To /tmp/racket-log (can `tail -f' it)
-              (with-output-to-file racket-log-file #:exists 'append
-                                   (lambda ()
-                                     (display (format "[~a] ~a\n" l m))))])
-           (loop)))))))
-
-(define (show-logger-levels)
-  (define wid 20)
-  (define (pr k v)
-    (printf "; ~a ~a\n"
-            (~a k
-                #:min-width wid
-                #:max-width wid
-                #:limit-marker "...")
-            v))
-  (pr "Logger" "Level")
-  (pr (make-string wid #\-) "-------")
-  (for ([(k v) logger-levels])
-    (pr k v))
-  (pr "[all other]" other-level)
-  (printf "; Writing ~v.\n" racket-log-file))
-
-(define (log-display specs)
-  (match specs
-    [(list) (show-logger-levels)]
-    [(list (and level (or 'none 'fatal 'error 'warning 'info 'debug)))
-     (set! other-level level)
-     (update-log-receiver)]
-    [(list logger 'default)
-     (hash-remove! logger-levels logger)
-     (update-log-receiver)]
-    [(list logger (and level (or 'none 'fatal 'error 'warning 'info 'debug)))
-     (hash-set! logger-levels logger level)
-     (update-log-receiver)]
-    [_ (eprintf
-        (string-join
-         '("; Usage:"
-           ",log                  -- show the levels currently in effect."
-           ",log <logger> <level> -- set logger to level debug|info|warning|error|fatal|none"
-           ",log <logger> default -- set logger to use the default, 'all other' level."
-           ",log <level>          -- set the default level, for 'all other' loggers.\n")
-         "\n; "))]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
