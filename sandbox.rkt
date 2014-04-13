12c4679d9139118dd761fbeb70532978448f0296
Greg Hendershott <greghendershott@gmail.com>
2014-04-12 22:36:47 -0400

    raise & with-handlers => channel-put & break-thread
    Don't need to use raise and with-handlers. Instead, sufficient to
    channel-put the message. Just also break (or suspend or kill the
    thread) so that it doesn't run a bit longer.
    
    Also: lambda -> λ

--- sandbox.rkt
+++ sandbox.rkt
@@ -19,7 +19,6 @@
   (define-values (path load-dir) (path-string->path&load-dir path-str))
   (define user-cust (make-custodian (current-custodian)))
   (define current-eventspace (txt/gui (make-parameter #f) current-eventspace))
-  (define ch (make-channel))
   (parameterize*
       ([current-custodian user-cust]
        [current-namespace ((txt/gui make-base-namespace make-gui-namespace))]
@@ -31,10 +30,9 @@
     ;; thread when racket/gui/base is not (yet) instantiated, or, from
     ;; (event-handler-thread (current-eventspace)).
     (define (repl-thunk)
-      (with-handlers ([msg?      (lambda (x) (channel-put ch x))]
-                      [exn:fail? (lambda (exn)
+      (with-handlers ([;; exn:fail during module load => re-run
+                       exn:fail? (λ (exn)
                                    (display-exn exn)
-                                   ;; exn:fail during module load => re-run
                                    (channel-put ch (rerun #f)))])
         (when (and path (module-path? path))
           (parameterize ([current-module-name-resolver repl-module-name-resolver])
@@ -43,9 +41,9 @@
         (parameterize ([current-prompt-read (make-prompt-read path)]
                        [error-display-handler our-error-display-handler]
                        [current-module-name-resolver repl-module-name-resolver])
-          ;; exn:fail during read-eval-print-loop => more r-e-p-l
+          ;; exn:fail during read-eval-print-loop => more cowbell
           (let repl ()
-            (with-handlers ([exn:fail? (lambda (exn) (display-exn exn) (repl))])
+            (with-handlers ([exn:fail? (λ (exn) (display-exn exn) (repl))])
               (read-eval-print-loop))))))
     ;; Main thread: Run repl-thunk on a plain thread, or, on the user
     ;; eventspace thread via queue-callback.
@@ -53,15 +51,25 @@
   ;; Main thread: Wait for message from REPL thread.
   (define msg (channel-get ch))
   (custodian-shutdown-all user-cust)
-  (newline)
+  (newline) ;; FIXME: Move this to racket-mode.el instead?
   (match msg
     [(rerun p)  (run p)]
     [(load-gui) (require-gui) (run path-str)]))
+
+
+(define ch (make-channel))
 
 ;; Messages via the channel from the repl thread to the main thread.
 (struct msg ())
 (struct rerun msg (path)) ;(or/c #f path-string?)
 (struct load-gui msg ())
+
+;; To be called from REPL thread. Puts message for the main thread
+;; to the channel, and does a break-thread (i.e. exit the thread with
+;; a return value).
+(define (put/break msg) ;; msg? -> any
+  (channel-put ch msg)
+  (break-thread (current-thread)))
 
 (define repl-module-name-resolver
   (let ([orig-resolver (current-module-name-resolver)])
@@ -71,7 +79,7 @@
       [(mp rmp stx load?)
        (when (and (eq? mp 'racket/gui/base) load?)
          (unless (gui-required?)
-           (raise (load-gui))))
+           (put/break (load-gui))))
        (orig-resolver mp rmp stx load?)])))
 
 ;; path-string? -> (values (or/c #f path?) path?)
@@ -91,7 +99,7 @@
   (define (not-found s)
     (eprintf "; ~a not found\n" s)
     #f)
-  (with-handlers ([exn:fail? (lambda (_) (not-found path-str))])
+  (with-handlers ([exn:fail? (λ (_) (not-found path-str))])
     (define path (expand-user-path (string->path path-str)))
     (cond [(file-exists? path) path]
           [else (not-found (path->string path))])))
@@ -99,11 +107,11 @@
 (define (make-prompt-read path)
   (define-values (base name dir?) (cond [path (split-path path)]
                                         [else (values "" "" #f)]))
-  (lambda ()
+  (λ ()
     (let loop ()
       (display name) (display "> ")
       (flush-output (current-error-port))
-      (with-handlers ([exn:fail? (lambda (exn)
+      (with-handlers ([exn:fail? (λ (exn)
                                    (display-exn exn)
                                    (loop))])
         (define in ((current-get-interaction-input-port)))
@@ -112,8 +120,8 @@
           [(uq cmd)
            (eq? 'unquote (syntax-e #'uq))
            (case (syntax-e #'cmd)
-             [(run) (raise (rerun (~a (read))))]
-             [(top) (raise (rerun #f))]
+             [(run) (put/break (rerun (~a (read))))]
+             [(top) (put/break (rerun #f))]
              [(def) (def (read))]
              [(doc) (doc (read-line))]
              [(exp) (exp1)]
