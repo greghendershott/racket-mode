greghendershott/racket-mode

commit 680b1b64585c33bdf0951b39db371d75a6447340

Add errortrace, profiling, coverage.
This commit is a rebase/squash of 46 commits over 1 week on the
`errortrace` branch, in preparation for rebasing to master and merging
to master.

- Add error-context-level, with values like low, medium, high, profile,
  coverage.

  - high = errortrace for better stack traces in error messages
  - profile = high + profile instrumentation
  - coverage = high + coverage instrumentation

  Only low, medium, high are in the Emacs UI. profile and coverage are
  used "internally" by racket-mode for temporary ,run commands.

- Add racket-profile command and racket-profile-mode for navigating the
  profile data and showing the source in other window. Also can eval
  more expressions in REPL and refresh the displayed profile data.

- With a C-u prefix, racket-test runs the tests with coverage
  instrumentation and highlights uncovered bits in the racket-mode
  buffer.

- To the existing `,command` syntax, add a `#,command` syntax. This says
  to redirect output to a file. This works more reliably than mixing
  command output in the REPL buffer -- especially when composing
  commands, like a ,run followed by a ,get-profile. This also means that
  the REPL command prompt doesn't need to use "unusual" characters in an
  effort to help `comint-redirect-send-command' work correctly.

cmds.rkt
@@ -1,6 +1,7 @@
-#lang racket/base
+#lang at-exp racket/base
 
-(require help/help-utils
+(require errortrace/errortrace-lib
+         help/help-utils
          macro-debugger/analysis/check-requires
          racket/contract
          racket/file
@@ -14,62 +15,103 @@
          racket/string
          syntax/modresolve
          (only-in xml xexpr->string)
+         "channel.rkt"
          "defn.rkt"
          "logger.rkt"
          "scribble.rkt"
          "try-catch.rkt"
          "util.rkt")
 
-(provide make-prompt-read)
+(provide make-prompt-read
+         current-command-output-file)
 
 (module+ test
   (require rackunit))
 
-(define (make-prompt-read path put/stop rerun)
+;; Commands intended for use programmatically by racket-mode may
+;; output their results to a file whose name is the value of the
+;; current-command-output-file parameter. This avoids mixing with
+;; stdout and stderr, which ultimately is not very reliable. How does
+;; racket-mode know when the file is ready to be read? 1. racket-mode
+;; deletes the file, calls us, and loops waiting for the file to
+;; exist. 2. We direct the command's output to a temporary file, then
+;; when the command has finished, rename the temp file to
+;; current-command-output-file. This way, racket-mode knows that as
+;; soon as the file exists again, the command is finished and its
+;; output is ready to be read from the file.
+(define current-command-output-file (make-parameter #f))
+(define tmp-file (make-temporary-file))
+(define (with-output-to-command-output-file f)
+  (cond [(current-command-output-file)
+         (with-output-to-file tmp-file f #:exists 'replace)
+         (rename-file-or-directory tmp-file (current-command-output-file) #t)]
+        [else (f)]))
+
+(define ((make-prompt-read path))
   (define-values (base name _) (cond [path (split-path path)]
                                      [else (values (current-directory) "" #f)]))
-  (λ ()
-    (let ([;; Elisp prints '() as 'nil. Reverse that. (Assumption:
-           ;; Although Elisp code puns nil/() also to mean "false",
-           ;; _our_ Elisp code _won't_ do that.)
-           read (λ () (match (read) ['nil '()] [x x]))])
-      (flush-output (current-error-port))
-      (display #\u227a) (display name) (display #\u227b) (display #\space)
-      (define in ((current-get-interaction-input-port)))
-      (define stx ((current-read-interaction) (object-name in) in))
-      (syntax-case stx ()
-        [(uq cmd)
-         (eq? 'unquote (syntax-e #'uq))
-         (case (syntax-e #'cmd)
-           [(run) (run put/stop rerun)]
-           [(top) (top put/stop rerun)]
-           [(def) (def (read))]
-           [(doc) (doc (read-syntax))]
-           [(describe) (describe (read-syntax))]
-           [(mod) (mod (read) path)]
-           [(type) (type (read))]
-           [(exp) (exp1 (read))]
-           [(exp+) (exp+)]
-           [(exp!) (exp! (read))]
-           [(log) (log-display (map string->symbol (string-split (read-line))))]
-           [(pwd) (display-commented (~v (current-directory)))]
-           [(cd) (cd (~a (read)))]
-           [(requires/tidy) (requires/tidy (read))]
-           [(requires/trim) (requires/trim (read) (read))]
-           [(requires/base) (requires/base (read) (read))]
-           [(find-collection) (find-collection (read))]
-           [(exit) (exit)]
-           [else (usage)])]
-        [_ stx]))))
+  (let ([read elisp-read])
+    (flush-output (current-error-port))
+    (display name) (display "> ")
+    (define in ((current-get-interaction-input-port)))
+    (define stx ((current-read-interaction) (object-name in) in))
+    (syntax-case stx ()
+      ;; #,command redirect
+      [(uq cmd)
+       (eq? 'unsyntax (syntax-e #'uq))
+       (with-output-to-command-output-file
+         (λ () (handle-command #'cmd path)))]
+      ;; ,command normal
+      [(uq cmd)
+       (eq? 'unquote (syntax-e #'uq))
+       (handle-command #'cmd path)]
+      [_ stx])))
+
+(define (elisp-read)
+  ;; Elisp prints '() as 'nil. Reverse that. (Assumption: Although
+  ;; some Elisp code puns nil/() also to mean "false", _our_ Elisp
+  ;; code _won't_ do that.)
+  (match (read)
+    ['nil '()]
+    [x x]))
+
+(define (handle-command cmd-stx path)
+  (case (syntax-e cmd-stx)
+    ;; These commands are intended to be used by either the user or
+    ;; racket-mode.
+    [(run) (run-or-top 'run)]
+    [(top) (run-or-top 'top)]
+    [(doc) (doc (read-syntax))]
+    [(exp) (exp1 (read))]
+    [(exp+) (exp+)]
+    [(exp!) (exp! (read))]
+    [(log) (log-display (map string->symbol (string-split (read-line))))]
+    [(pwd) (display-commented (~v (current-directory)))]
+    [(cd) (cd (~a (read)))]
+    [(exit) (exit)]
+    [(info) (info)]
+    ;; These remaining commands are intended to be used by
+    ;; racket-mode, only.
+    [(syms) (syms)]
+    [(def) (def (read))]
+    [(describe) (describe (read-syntax))]
+    [(mod) (mod (read) path)]
+    [(type) (type (read))]
+    [(requires/tidy) (requires/tidy (read))]
+    [(requires/trim) (requires/trim (read) (read))]
+    [(requires/base) (requires/base (read) (read))]
+    [(find-collection) (find-collection (read))]
+    [(get-profile) (get-profile)]
+    [(get-uncovered) (get-uncovered path)]
+    [else (usage)]))
 
 (define (usage)
-  (displayln
+  (display-commented
    "Commands:
-,run </path/to/file.rkt> [<memory-limit-MB> [<pretty-print?>]]
-,top [<memory-limit-MB>]
+,run </path/to/file.rkt> [<memory-limit-MB> [<pretty-print?> [<error-context>]]]
+   where <error-context> = low | medium | high
+,top [<memory-limit-MB> [<pretty-print?> [<error-context>]]]
 ,exit
-,def <identifier>
-,type <identifier>
 ,doc <identifier>|<string>
 ,exp <stx>
 ,exp+
@@ -79,11 +121,11 @@
 ,log <opts> ...")
   (void))
 
-;;; run and top
+;;; run, top, info
 
-;; Parameter-like interface, but we don't care about thread-local
-;; stuff. We do care about calling collect-garbage IFF the new limit
-;; is less than the old one or less than the current actual usage.
+;; Parameter-like interface, but we don't want thread-local. We do
+;; want to call collect-garbage IFF the new limit is less than the old
+;; one or less than the current actual usage.
 (define current-mem
   (let ([old #f])
     (case-lambda
@@ -95,41 +137,48 @@
             (collect-garbage))
        (set! old new)])))
 
-(define current-pp? (make-parameter #t))
+;; Likewise: Want parameter signature but NOT thread-local.
+(define-syntax-rule (make-parameter-ish init)
+  (let ([old init])
+    (case-lambda
+      [() old]
+      [(new) (set! old new)])))
 
-(define (run put/stop rerun)
-  ;; Note: Use ~a on path to allow both `,run "/path/file.rkt"` and
-  ;; `run /path/file.rkt`.
+(define current-pp? (make-parameter-ish #t))
+(define current-ctx-lvl (make-parameter-ish 'low)) ;context-level?
+
+(define (run-or-top which)
+  ;; Support both the ,run and ,top commands. Latter has no first path
+  ;; arg, but otherwise they share subsequent optional args. (Note:
+  ;; The complexity here is from the desire to let user type simply
+  ;; e.g. ",top" or ",run file" and use the existing values for the
+  ;; omitted args. We're intended mainly to be used from Emacs, which
+  ;; can/does always supply all the args. But, may as well make it
+  ;; convenient for human users, too.)
   (define (go path)
-    (put/stop (rerun (~a path) (current-mem) (current-pp?))))
-  (match (read-line->reads)
-    [(list path mem pp?) (cond [(and (number? mem) (boolean? pp?))
-                                (current-mem mem)
-                                (current-pp? pp?)
-                                (go path)]
-                               [else (usage)])]
-    [(list path mem)     (cond [(number? mem)
-                                (current-mem mem)
-                                (go path)]
-                               [else (usage)])]
-    [(list path)         (go path)]
-    [_                   (usage)]))
-
-(define (top put/stop rerun)
-  (define (go)
-    (put/stop (rerun #f (current-mem) (current-pp?))))
-  (match (read-line->reads)
-    [(list mem pp?) (cond [(and (number? mem) (boolean? pp?))
-                           (current-mem mem)
-                           (current-pp? pp?)
-                           (go)]
-                          [else (usage)])]
-    [(list mem)     (cond [(number? mem)
-                           (current-mem mem)
-                           (go)]
-                          [else (usage)])]
-    [(list)         (go)]
-    [_              (usage)]))
+    (put/stop (rerun (and path (~a path)) ;"/path/file" or /path/file
+                     (current-mem)
+                     (current-pp?)
+                     (current-ctx-lvl))))
+  (match (match which
+           ['run (read-line->reads)]
+           ['top (cons #f (read-line->reads))]) ;i.e. path = #f
+    [(list path (? number? mem) (? boolean? pp?) (? context-level? ctx))
+     (current-mem mem)
+     (current-pp? pp?)
+     (current-ctx-lvl ctx)
+     (go path)]
+    [(list path (? number? mem) (? boolean? pp?))
+     (current-mem mem)
+     (current-pp? pp?)
+     (go path)]
+    [(list path (? number? mem) (? boolean? pp?))
+     (current-mem mem)
+     (go path)]
+    [(list path)
+     (go path)]
+    [_
+     (usage)]))
 
 (define (read-line->reads)
   (reads-from-string (read-line)))
@@ -144,7 +193,17 @@
     ['nil            (cons #f (reads))] ;in case from elisp
     [x               (cons x (reads))]))
 
+;; This really just for my own use debugging. Not documented.
+(define (info)
+  (displayln @~a{Memory Limit:   @(current-mem)
+                 Pretty Print:   @(current-pp?)
+                 Error Context:  @(current-ctx-lvl)
+                 Command Output: @(current-command-output-file)}))
+
 ;;; misc other commands
+
+(define (syms)
+  (elisp-println (map symbol->string (namespace-mapped-symbols))))
 
 (define (def sym)
   (elisp-println (find-definition (symbol->string sym))))
@@ -210,14 +269,8 @@
 ;; (because the argument names have explanatory value), and also look
 ;; for Typed Racket type or a contract, if any.
 
-;; Keep reusing one temporary file.
-(define describe-html-path (make-temporary-file "racket-mode-describe-~a"))
-
 (define (describe stx)
-  (with-output-to-file describe-html-path #:mode 'text #:exists 'replace
-    (λ () (display (describe* stx))))
-  (elisp-println (path->string describe-html-path))
-  (void)) ;(void) prevents Typed Racket type annotation line
+  (display (describe* stx)))
 
 (define (describe* _stx)
   (define stx (namespace-syntax-introduce _stx))
@@ -248,8 +301,9 @@
 ;;; misc
 
 (define (doc stx)
-  (cond [(identifier? stx) (find-help (namespace-syntax-introduce stx))]
-        [else              (search-for (list (~a (syntax->datum stx))))]))
+  (try
+   (find-help (namespace-syntax-introduce stx))
+   #:catch exn:fail? _ (search-for (list (~a (syntax->datum stx))))))
 
 (define (cd s)
   (let ([old-wd (current-directory)])
@@ -547,6 +601,66 @@
     [f  (map path->string (f str))]))
 
 (define find-collection (compose elisp-println do-find-collection))
+
+;;; profile
+
+(define (get-profile)
+  (elisp-println
+   ;; TODO: Filter files from racket-mode itself, b/c just noise?
+   (for/list ([x (in-list (get-profile-results))])
+     (match-define (list count msec name stx _ ...) x)
+     (list count
+           msec
+           (and name (symbol->string name))
+           (and (syntax-source stx) (path? (syntax-source stx))
+                (path->string (syntax-source stx)))
+           (syntax-position stx)
+           (and (syntax-position stx) (syntax-span stx)
+                (+ (syntax-position stx) (syntax-span stx)))))))
+
+;;; coverage
+
+(define (get-uncovered path)
+  (elisp-println
+   (consolidate-coverage-ranges
+    (for*/list ([x (in-list (get-execute-counts))]
+                [stx (in-value (car x))]
+                #:when (equal? path (syntax-source stx))
+                [count (in-value (cdr x))]
+                #:when (zero? count))
+      (cons (syntax-position stx)
+            (+ (syntax-position stx) (syntax-span stx)))))))
+
+(define (consolidate-coverage-ranges xs)
+  (remove-duplicates (sort xs < #:key car)
+                     same?))
+
+(define (same? x y)
+  ;; Is x a subset of y or vice versa?
+  (match-define (cons x/beg x/end) x)
+  (match-define (cons y/beg y/end) y)
+  (or (and (<= x/beg y/beg) (<= y/end x/end))
+      (and (<= y/beg x/beg) (<= x/end y/end))))
+
+(module+ test
+  (check-true (same? '(0 . 9) '(0 . 9)))
+  (check-true (same? '(0 . 9) '(4 . 5)))
+  (check-true (same? '(4 . 5) '(0 . 9)))
+  (check-false (same? '(0 . 1) '(1 . 2)))
+  (check-equal? (consolidate-coverage-ranges
+                 '((10 . 20) (10 . 11) (19 . 20) (10 . 20)
+                   (20 . 30) (20 . 21) (29 . 30) (20 . 30)))
+                '((10 . 20)
+                  (20 . 30)))
+  ;; This is a test of actual coverage data I got from one example,
+  ;; where the maximal subsets were (164 . 197) and (214. 247).
+  (check-equal?
+   (consolidate-coverage-ranges
+    '((164 . 197) (164 . 197) (164 . 197)
+      (173 . 180) (173 . 180) (173 . 180) (173 . 180) (173 . 180) (187 . 196)
+      (214 . 247) (214 . 247) (214 . 247)
+      (223 . 230) (223 . 230) (223 . 230) (223 . 230) (223 . 230) (237 . 246)))
+   '((164 . 197) (214 . 247))))
 
 ;; Local Variables:
 ;; coding: utf-8

channel.rkt
@@ -1,0 +1,61 @@
+#lang racket/base
+
+(require racket/match
+         racket/contract)
+
+(provide the-channel
+         (struct-out msg)
+         (struct-out load-gui)
+         (struct-out rerun)
+         rerun-default
+         context-level?
+         errortrace-level?
+         profile/coverage-level?
+         put/stop)
+
+;; Definitions for the context-level member of rerun
+
+(define profile/coverage-levels
+  ;; "sibling" levels that all require errortrace
+  '(profile    ;high with profiling-enabled
+    coverage)) ;high with execute-counts-enabled
+
+(define errortrace-levels
+  `(high     ;compile-context-preservation-enabled #t + errortrace
+    ,@profile/coverage-levels))
+
+(define context-levels
+  `(low      ;compile-context-preservation-enabled #f
+    medium   ;compile-context-preservation-enabled #t
+    ,@errortrace-levels))
+
+(define-syntax-rule (memq? x xs)
+  (not (not (memq x xs))))
+
+(define (context-level? v)
+  (memq? v context-levels))
+
+(define (errortrace-level? v)
+  (memq? v errortrace-levels))
+
+(define (profile/coverage-level? v)
+  (memq? v profile/coverage-levels))
+
+;; Messages via a channel from the repl thread to the main thread.
+(define the-channel (make-channel))
+(define-struct/contract msg ())
+(define-struct/contract [load-gui msg] ())
+(define-struct/contract [rerun msg]
+  ([path          (or/c #f path-string?)]
+   [memory-limit  (or/c #f exact-positive-integer?)]
+   [pretty-print? boolean?]
+   [context-level context-level?]))
+
+(define rerun-default (rerun #f #f #f 'low))
+
+;; To be called from REPL thread. Puts message for the main thread to
+;; the channel, and blocks itself; main thread will kill the REPL
+;; thread. Effectively "exit the thread with a return value".
+(define (put/stop v) ;; msg? -> void?
+  (channel-put the-channel v)
+  (void (sync never-evt)))

error.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
 
-(require racket/match
+(require errortrace/errortrace-lib
+         racket/match
          racket/runtime-path
          racket/string
          "util.rkt"
@@ -38,21 +39,29 @@
         (display-commented (source-location->string srcloc))))))
 
 (define (display-context exn)
-  (match (context->string
-          (continuation-mark-set->context (exn-continuation-marks exn)))
-    ["" (void)]
-    [s (display-commented "Context:")
-       (display-commented s)]))
+  (cond [(instrumenting-enabled)
+         (define p (open-output-string))
+         (print-error-trace p exn)
+         (match (get-output-string p)
+           ["" (void)]
+           [s  (display-commented (string-append "Context (errortrace):"
+                                                 ;; et prepends a \n
+                                                 s))])]
+        [else
+         (match (context->string
+                 (continuation-mark-set->context (exn-continuation-marks exn)))
+           ["" (void)]
+           [s (display-commented (string-append "Context:\n"
+                                                s))])]))
 
 (define (context->string xs)
   ;; Limit the context in two ways:
   ;; 1. Don't go beyond error-print-context-length
   ;; 2. Don't go into "system" context that's just noisy.
-  ;; Also, show the context in reverse, for Emacs compilation-mode.
-  (string-join (reverse (for/list ([x xs]
-                                   [_ (error-print-context-length)]
-                                   #:unless (system-context? x))
-                          (context-item->string x)))
+  (string-join (for/list ([x xs]
+                          [_ (error-print-context-length)]
+                          #:unless (system-context? x))
+                 (context-item->string x))
                "\n"))
 
 (define-runtime-path run.rkt "run.rkt")

makefile
@@ -21,12 +21,12 @@ compile: clean \
 	racket-collection.elc \
 	racket-complete.elc \
 	racket-edit.elc \
-	racket-eval.elc \
 	racket-font-lock.elc \
 	racket-indent.elc \
 	racket-keywords-and-builtins.elc \
 	racket-make-doc.elc \
 	racket-mode.elc \
+	racket-profile.elc \
 	racket-repl.elc \
 	racket-tests.elc \
 	racket-util.elc

older-racket.rkt
@@ -8,7 +8,11 @@
 ;; run on Racket 5.3.5 (perhaps earlier, but I haven't tested).
 (provide find-collects-dir
          path->collects-relative
-         current-directory-for-user)
+         current-directory-for-user
+         hash-clear!)
+
+(module+ test
+  (require rackunit))
 
 (define (our-find-collects-dir)
   (apply build-path
@@ -44,6 +48,21 @@
 (define current-directory-for-user
   (dynamic-require 'racket/base 'current-directory-for-user
                    (const our-current-directory-for-user)))
+
+;;; Racket 6.0 adds hash-clear!
+
+(define (our-hash-clear! ht)
+  (for ([key (in-list (hash-keys ht))])
+    (hash-remove! ht key)))
+
+(module+ test
+  (define ht (make-hash '((0 . "zero") (1 . "one") (2 . "two"))))
+  (our-hash-clear! ht)
+  (check-true (zero? (hash-count ht))))
+
+(define hash-clear!
+  (dynamic-require 'racket/base 'hash-clear!
+                   (const our-hash-clear!)))
 
 ;; Local Variables:
 ;; coding: utf-8

racket-collection.el
@@ -17,7 +17,7 @@
 ;; http://www.gnu.org/licenses/ for details.
 
 (require 'racket-util)
-(require 'racket-eval)
+(require 'racket-repl)
 (require 'ido)
 (require 'tq)
 (require 'racket-custom) ;for `racket-racket-program'
@@ -48,7 +48,7 @@ Also handy is the `flx-ido' package from MELPA.
 See also: `racket-visit-module' and `racket-open-require-path'."
   (interactive "P")
   (let* ((coll  (racket--symbol-at-point-or-prompt prefix "Collection name: "))
-         (paths (racket--eval/sexpr (format ",find-collection \"%s\"\n" coll))))
+         (paths (racket--repl-cmd/sexpr (format ",find-collection \"%s\"\n" coll))))
     (cond ((eq 'find-collection-not-installed paths)
            ;; FIXME? Offer to run this for them?
            (error "Run `raco pkg install raco-find-collection'"))

racket-complete.el
@@ -18,7 +18,7 @@
 
 (require 'cl-lib)
 (require 'racket-custom)
-(require 'racket-eval)
+(require 'racket-repl)
 
 (make-variable-buffer-local
  (defvar racket--namespace-symbols nil
@@ -35,9 +35,7 @@ See `racket--invalidate-completion-cache' and
   "Get Racket namespace symbols from the cache or from the Racket process."
   (unless racket--namespace-symbols
     (setq racket--namespace-symbols
-          (racket--eval/sexpr
-           (format "%S"
-                   `(map symbol->string (namespace-mapped-symbols))))))
+          (racket--repl-cmd/sexpr ",syms")))
   racket--namespace-symbols)
 
 (defun racket--complete-prefix (prefix)
@@ -125,7 +123,7 @@ See `racket--invalidate-completion-cache' and
     ;; ...and therefore this test:
     (if (not (eq v 'not-found))
         v
-      (let ((v (racket--eval/sexpr (concat ",type " str))))
+      (let ((v (racket--repl-cmd/sexpr (concat ",type " str))))
         (puthash sym v racket--type-cache)
         v))))
 

racket-custom.el
@@ -61,6 +61,33 @@ the limit (maybe by a significant amount)."
   :safe #'integerp
   :group 'racket)
 
+(defcustom racket-error-context 'medium
+  "The level of context used for `racket-run' error stack traces.
+
+Each level improves stack trace information, but causes your
+program to run more slowly.
+
+  - 'low corresponds to `compile-context-preservation-enabled`
+    `#f`.
+
+  - 'medium corresponds to `compile-context-preservation-enabled`
+    `#t`, which disables some optimizations like inlining.
+
+  - 'high corresponds to `compile-context-preservation-enabled`
+    `#t` and to use of `errortrace`, which heavily instruments
+    your code and therefore may be significantly slower.
+
+Tip: Regardless of this setting, you can enable 'high errortrace
+for a specific `racket-run' using a C-u prefix. This lets you
+normally run with a faster setting, and temporarily re-run to get
+a more-helpful error message."
+  :tag "Error Context"
+  :type '(radio (const :tag "Low" low)
+                (const :tag "Medium (slower)" medium)
+                (const :tag "High (much slower)" high))
+  :risky t
+  :group 'racket)
+
 ;;; REPL
 
 (defgroup racket-repl nil

commit 48fde3e8cdb45b2a24d56b36bb7bbc4b49b7de68

handle-command: read should be elisp-read. Fixes #116.
This broke when refactoring in commit 680b1b6. The

    (let ([read elisp-read] ...))

should have been moved into handle-command, too.

I'd actually noticed and fixed this mistake on a topic branch, recently.
I hadn't seen any symptoms, so I didn't think it was urgent to do this
on master. I was wrong.

cmds.rkt
@@ -48,22 +48,21 @@
 (define ((make-prompt-read path))
   (define-values (base name _) (cond [path (split-path path)]
                                      [else (values (current-directory) "" #f)]))
-  (let ([read elisp-read])
-    (flush-output (current-error-port))
-    (display name) (display "> ")
-    (define in ((current-get-interaction-input-port)))
-    (define stx ((current-read-interaction) (object-name in) in))
-    (syntax-case stx ()
-      ;; #,command redirect
-      [(uq cmd)
-       (eq? 'unsyntax (syntax-e #'uq))
-       (with-output-to-command-output-file
-         (λ () (handle-command #'cmd path)))]
-      ;; ,command normal
-      [(uq cmd)
-       (eq? 'unquote (syntax-e #'uq))
-       (handle-command #'cmd path)]
-      [_ stx])))
+  (flush-output (current-error-port))
+  (display name) (display "> ")
+  (define in ((current-get-interaction-input-port)))
+  (define stx ((current-read-interaction) (object-name in) in))
+  (syntax-case stx ()
+    ;; #,command redirect
+    [(uq cmd)
+     (eq? 'unsyntax (syntax-e #'uq))
+     (with-output-to-command-output-file
+       (λ () (handle-command #'cmd path)))]
+    ;; ,command normal
+    [(uq cmd)
+     (eq? 'unquote (syntax-e #'uq))
+     (handle-command #'cmd path)]
+    [_ stx]))
 
 (define (elisp-read)
   ;; Elisp prints '() as 'nil. Reverse that. (Assumption: Although
@@ -74,36 +73,37 @@
     [x x]))
 
 (define (handle-command cmd-stx path)
-  (case (syntax-e cmd-stx)
-    ;; These commands are intended to be used by either the user or
-    ;; racket-mode.
-    [(run) (run-or-top 'run)]
-    [(top) (run-or-top 'top)]
-    [(doc) (doc (read-syntax))]
-    [(exp) (exp1 (read))]
-    [(exp+) (exp+)]
-    [(exp!) (exp! (read))]
-    [(log) (log-display (map string->symbol (string-split (read-line))))]
-    [(pwd) (display-commented (~v (current-directory)))]
-    [(cd) (cd (~a (read)))]
-    [(exit) (exit)]
-    [(info) (info)]
-    ;; These remaining commands are intended to be used by
-    ;; racket-mode, only.
-    [(path) (elisp-println (and path (path->string path)))]
-    [(syms) (syms)]
-    [(def) (def (read))]
-    [(describe) (describe (read-syntax))]
-    [(mod) (mod (read) path)]
-    [(type) (type (read))]
-    [(requires/tidy) (requires/tidy (read))]
-    [(requires/trim) (requires/trim (read) (read))]
-    [(requires/base) (requires/base (read) (read))]
-    [(find-collection) (find-collection (read))]
-    [(get-profile) (get-profile)]
-    [(get-uncovered) (get-uncovered path)]
-    [(check-syntax) (check-syntax path)]
-    [else (usage)]))
+  (let ([read elisp-read])
+    (case (syntax-e cmd-stx)
+      ;; These commands are intended to be used by either the user or
+      ;; racket-mode.
+      [(run) (run-or-top 'run)]
+      [(top) (run-or-top 'top)]
+      [(doc) (doc (read-syntax))]
+      [(exp) (exp1 (read))]
+      [(exp+) (exp+)]
+      [(exp!) (exp! (read))]
+      [(log) (log-display (map string->symbol (string-split (read-line))))]
+      [(pwd) (display-commented (~v (current-directory)))]
+      [(cd) (cd (~a (read)))]
+      [(exit) (exit)]
+      [(info) (info)]
+      ;; These remaining commands are intended to be used by
+      ;; racket-mode, only.
+      [(path) (elisp-println (and path (path->string path)))]
+      [(syms) (syms)]
+      [(def) (def (read))]
+      [(describe) (describe (read-syntax))]
+      [(mod) (mod (read) path)]
+      [(type) (type (read))]
+      [(requires/tidy) (requires/tidy (read))]
+      [(requires/trim) (requires/trim (read) (read))]
+      [(requires/base) (requires/base (read) (read))]
+      [(find-collection) (find-collection (read))]
+      [(get-profile) (get-profile)]
+      [(get-uncovered) (get-uncovered path)]
+      [(check-syntax) (check-syntax path)]
+      [else (usage)])))
 
 (define (usage)
   (display-commented