36bd12f8ad755f9e13c18d061b611e7af0f4490e
Greg Hendershott <greghendershott@gmail.com>
2015-07-15 11:56:20 -0400

    makefile-mode needs indent-tabs-mode t

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,6 +2,8 @@
   . ((indent-tabs-mode         . nil)
      (require-final-newline    . t)
      (show-trailing-whitespace . t)))
+ (makefile-mode
+  . ((indent-tabs-mode . t)))
  (prog-mode
   . ((comment-column . 40)
      (fill-column    . 78))))
