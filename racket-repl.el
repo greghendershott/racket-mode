2f90f3dcc8bc0a3510276ad3f14b24c379d78293
Greg Hendershott <greghendershott@gmail.com>
2015-11-01 15:15:53 -0500

    Simplify avoiding comint-highlight-input
    Instead of a copypasta of comint-send-input minus the expression to add
    'font-lock-face 'comint-highlight-input -- simply call comint-send-input
    then immediately remove the property afterwards. Although I see it
    briefly redisplay with the highlight when stepping in edebug, I don't in
    normal use. Even if it were to flash momentarily, that wouldn't
    necessarily look bad AFAICT.

--- racket-repl.el
+++ racket-repl.el
@@ -46,92 +46,6 @@
         (backward-sexp)
         (buffer-substring (point) end)))))
 
-;; I don't want comint-mode clobbering our font-lock with
-;; comint-highlight-input face. Changing that _face_ to be non-bold
-;; isn't sufficient, it simply clobbers everything non-bold.
-;;
-;; So far, the least-pukey way I can figure out how to do this is to
-;; copy-pasta much of comint-send-input, and modify the one tiny
-;; offending bit.  Blech. If anyone reading this knows a better way,
-;; please let me know!
-;;
-;; Update: I went on to slim down the copy -- deleted the `no-newline`
-;; and `artificial` args we don't use, and the code that could only
-;; execute if they were non-nil.
-(defun racket--comint-send-input ()
-  "Like `comint-send-input` but doesn't use face `comint-highlight-input'."
-  ;; Note that the input string does not include its terminal newline.
-  (let ((proc (get-buffer-process (current-buffer))))
-    (if (not proc) (user-error "Current buffer has no process")
-      (widen)
-      (let* ((pmark (process-mark proc))
-             (intxt (if (>= (point) (marker-position pmark))
-                        (progn (if comint-eol-on-send (end-of-line))
-                               (buffer-substring pmark (point)))
-                      (let ((copy (racket--get-old-input)))
-                        (goto-char pmark)
-                        (insert copy)
-                        copy)))
-             (input (if (not (eq comint-input-autoexpand 'input))
-                        ;; Just whatever's already there.
-                        intxt
-                      ;; Expand and leave it visible in buffer.
-                      (comint-replace-by-expanded-history t pmark)
-                      (buffer-substring pmark (point))))
-             (history (if (not (eq comint-input-autoexpand 'history))
-                          input
-                        ;; This is messy 'cos ultimately the original
-                        ;; functions used do insertion, rather than return
-                        ;; strings.  We have to expand, then insert back.
-                        (comint-replace-by-expanded-history t pmark)
-                        (let ((copy (buffer-substring pmark (point)))
-                              (start (point)))
-                          (insert input)
-                          (delete-region pmark start)
-                          copy))))
-        (insert ?\n)
-        (comint-add-to-input-history history)
-        (run-hook-with-args 'comint-input-filter-functions
-                            (concat input "\n"))
-        (let ((beg (marker-position pmark))
-              (end (1- (point)))
-              (inhibit-modification-hooks t))
-          (when (> end beg)
-            ;;;; The bit from comint-send-input that we DON'T want:
-            ;; (add-text-properties beg end
-            ;;                      '(front-sticky t
-            ;;                        font-lock-face comint-highlight-input))
-            (unless comint-use-prompt-regexp
-              ;; Give old user input a field property of `input', to
-              ;; distinguish it from both process output and unsent
-              ;; input.  The terminating newline is put into a special
-              ;; `boundary' field to make cursor movement between input
-              ;; and output fields smoother.
-              (add-text-properties
-               beg end
-               '(mouse-face highlight
-                 help-echo "mouse-2: insert after prompt as new input"))))
-          (unless comint-use-prompt-regexp
-            ;; Cover the terminating newline
-            (add-text-properties end (1+ end)
-                                 '(rear-nonsticky t
-                                   field boundary
-                                   inhibit-line-move-field-capture t))))
-        (comint-snapshot-last-prompt)
-        (setq comint-save-input-ring-index comint-input-ring-index)
-        (setq comint-input-ring-index nil)
-        ;; Update the markers before we send the input
-        ;; in case we get output amidst sending the input.
-        (set-marker comint-last-input-start pmark)
-        (set-marker comint-last-input-end (point))
-        (set-marker (process-mark proc) (point))
-        ;; clear the "accumulation" marker
-        (set-marker comint-accum-marker nil)
-        (funcall comint-input-sender proc input)
-        ;; This used to call comint-output-filter-functions,
-        ;; but that scrolled the buffer in undesirable ways.
-        (run-hook-with-args 'comint-output-filter-functions "")))))
-
 (defun racket-repl-eval-or-newline-and-indent ()
   "If complete sexpr, eval in Racket. Else do `racket-newline-and-indent'."
   (interactive)
@@ -139,12 +53,16 @@
     (cond ((not proc) (user-error "Current buffer has no process"))
           ((not (eq "" (racket--get-old-input)))
            (condition-case nil
-               (progn
-                 (save-excursion
-                   (goto-char (process-mark proc))
-                   (forward-list)) ;will error unless complete sexpr
-                 (racket--comint-send-input))
-             (error (newline-and-indent)))))))
+               (let* ((beg (marker-position (process-mark proc)))
+                      (end (save-excursion
+                             (goto-char beg)
+                             (forward-list) ;scan-error unless complete sexpr
+                             (point))))
+                 (comint-send-input)
+                 ;; Remove comint-highlight-input face applied to
+                 ;; input. I don't like how that looks.
+                 (remove-text-properties beg end '(font-lock-face comint-highlight-input)))
+             (scan-error (newline-and-indent)))))))
 
 ;;;###autoload
 (defun racket-repl (&optional noselect)
