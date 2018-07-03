a4c76f5b061630d69df6d7bcb57781e2e3f372c1
Greg Hendershott <greghendershott@gmail.com>
2018-07-03 10:22:09 -0400

    Fix typo

--- run.rkt
+++ run.rkt
@@ -164,7 +164,7 @@
   ;; Check that the lang defines #%top-interaction
   (unless (memq '#%top-interaction (namespace-mapped-symbols))
     (display-commented
-     "Because the language used by this module provides no `#%top-interaction\n you will be unable to evaluate expressions here in the REPL.")))
+     "Because the language used by this module provides no #%top-interaction\n you will be unable to evaluate expressions here in the REPL.")))
 
 ;; Catch attempt to load racket/gui/base for the first time.
 (define repl-module-name-resolver
