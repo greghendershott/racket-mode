greghendershott/racket-mode

commit 7140f6116450ff9531923508dc9dd5016ed9ab59

path+md5 should always return Unix-style paths; fixes #346
Here "Unix-style paths" specifically includes the Emacs flavor where
it treats Windows paths like c:\path\to\file.rkt as
c:/path/to/file.rkt.

racket/elisp.rkt
@@ -3,7 +3,8 @@
 (require racket/contract
          racket/match
          racket/port
-         racket/set)
+         racket/set
+         racket/string)
 
 (provide elisp-read
          elisp-writeln
@@ -43,7 +44,7 @@
     [#t             't]
     [(? list? xs)   (map racket->elisp xs)]
     [(cons x y)     (cons (racket->elisp x) (racket->elisp y))]
-    [(? path? v)    (path->string v)]
+    [(? path? v)    (path->string/emacs v)]
     [(? hash? v)    (for/list ([(k v) (in-hash v)])
                       (cons (racket->elisp k) (racket->elisp v)))]
     [(? set? v)     (map racket->elisp (set->list v))]
@@ -55,3 +56,12 @@
                   (λ () (elisp-write '(1 #t nil () (a . b) #hash((1 . 2) (3 . 4)))
                                      (current-output-port))))
                 "(1 t nil nil (a . b) ((1 . 2) (3 . 4)))"))
+
+(define (path->string/emacs p)
+  (string-join
+   (match (map path->string (explode-path p))
+     [(list* (pregexp "([a-zA-z]:)\\\\" (list _ drive)) vs)
+      (cons drive vs)]
+     [vs vs])
+   "/"))
+

commit f7d37a5f12832e1a821516c54ef4e515f713336b

Properly handle Unix style paths; fixes #357
Commit 7140f61 broke this trying to fix #346.

racket/elisp.rkt
@@ -58,10 +58,15 @@
                 "(1 t nil nil (a . b) ((1 . 2) (3 . 4)))"))
 
 (define (path->string/emacs p)
-  (string-join
-   (match (map path->string (explode-path p))
-     [(list* (pregexp "([a-zA-z]:)\\\\" (list _ drive)) vs)
-      (cons drive vs)]
-     [vs vs])
-   "/"))
+  (match (map path->string (explode-path p))
+    [(list* (pregexp "([a-zA-z]:)\\\\" (list _ drive)) vs)
+     #:when (eq? 'windows (system-type))
+     (string-join (cons drive vs))]
+    [_ p]))
 
+(module+ test
+  (when (eq? 'windows (system-type))
+    (check-equal? (path->string/emacs "C:\\path\\to\\foo.rkt")
+                  "C:/path/to/foo.rkt"))
+  (check-equal? (path->string/emacs "/path/to/foo.rkt")
+                "/path/to/foo.rkt"))