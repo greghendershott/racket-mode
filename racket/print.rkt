bf31305d903b375f2be88d99f87891843a604e5d
Greg Hendershott <git@greghendershott.com>
2019-09-25 10:46:53 -0400

    Simplify pretty-print-size-hook; fixes #402

--- racket/print.rkt
+++ racket/print.rkt
@@ -20,11 +20,18 @@
           (print (convert-image v))
           (newline))))
 
+;; We have nothing valuable to say about (a) pretty-print-columns as
+;; it relates to Emacs window width or (b) the width in "characters"
+;; of any particular image. Even so, we must say something. We must
+;; supply a pretty-print-size-hook that returns _some_ integer width
+;; -- else our pretty-print-hook won't be called. So simply return
+;; pretty-print-columns. This means that e.g. a list of images will be
+;; printed on multiple lines even if they could fit on one line.
+;; That's less-worse than trying to be "clever"; see issue #402.
 (define (make-pretty-print-size-hook)
-  (let ([orig (pretty-print-size-hook)]
-        [width (floor (/ (pretty-print-columns) 4))]) ;magic number? yep.
+  (let ([orig (pretty-print-size-hook)])
     (λ (value display? port)
-      (cond [(convert-image? value) width]
+      (cond [(convert-image? value) (pretty-print-columns)]
             [else                   (orig value display? port)]))))
 
 (define (make-pretty-print-print-hook)
