9f70e5b005e48487a1d72808da0fd855effc9b96
Greg Hendershott <git@greghendershott.com>
2020-02-11 13:36:17 -0500

    Polish some comments and code

--- racket/commands/help.rkt
+++ racket/commands/help.rkt
@@ -1,45 +1,51 @@
 #lang at-exp racket/base
 
-(require scribble/xref
-         (only-in scribble/core tag?)
+(require (only-in scribble/core tag?)
+         scribble/xref
          setup/dirs
          setup/xref
          net/uri-codec
          racket/contract
          racket/format
          racket/match
-         "../identifier.rkt")
+        "../identifier.rkt")
 
 (provide doc)
 
-;; Once upon a time, you could enter commands in the REPL like ",doc"
-;; and it used to make sense to open the browser, here, and have a lot
-;; of crap to do so on macOS to handle anchors a.k.a. fragments.
+;; Once upon a time, you could enter commands in the REPL like ",doc".
+;; It made sense to open the browser here -- despite needing junk to
+;; convince macOS to open a file: URL using anchors a.k.a. fragments.
 ;;
-;; But nowadays just return the URL and let Emacs open the browser.
-;; Especially because now check-syntax sometimes returns path + anchor
-;; and we need to use that approach. Better to open the browser just
-;; one way.
+;; But nowadays? Just return the URL. Let Emacs open the browser.
+;; Especially because now check-syntax sometimes returns a help URL,
+;; in which case the front end should just open the browser. Given
+;; that case, let's always open the browser one consistent way -- in
+;; Emacs using browse-url.
+
+;; We are lazy-required so `delay`-ing would be N/A.
+(define xref (load-collections-xref))
 
 (define/contract (doc how str)
   (-> how/c string? string?)
-  (->identifier how str stx->uri-string))
+  (or (->identifier how str stx->uri-string)
+      (search str)))
 
 (define (stx->uri-string stx)
-  (define xref (load-collections-xref))
   (~a "file://"
       (match (and xref (xref-binding->definition-tag xref stx 0))
         [(? tag? tag)
          (define-values (path anchor) (xref-tag->path+anchor xref tag))
          (~a path "#" anchor)]
-        [_
-         (~a (for/or ([f (in-list (list find-user-doc-dir find-doc-dir))])
-               (search-dir f))
-             "?q="
-             (uri-encode (~a (syntax->datum stx))))])))
+        [_ (search (~a (syntax->datum stx)))])))
+
+(define (search str)
+  (~a (for/or ([f (in-list (list find-user-doc-dir find-doc-dir))])
+        (search-dir f))
+      "?q="
+      (uri-encode str)))
 
 (define (search-dir f)
   (match (f)
-    [#f #f]
-    [dir (define path (build-path dir "search/index.html"))
-         (and (file-exists? path) (path->string path))]))
+    [(? path? dir) (define path (build-path dir "search/index.html"))
+                   (and (file-exists? path) (path->string path))]
+    [_ #f]))
