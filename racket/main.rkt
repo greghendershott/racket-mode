f0f47cfeb35a24a819102da6e54ec4025ab0a2d0
Greg Hendershott <git@greghendershott.com>
2020-03-23 11:35:25 -0400

    Tweak startup code
    Not (supposed to be) a substantive change.

--- racket/main.rkt
+++ racket/main.rkt
@@ -23,21 +23,13 @@
        (eprintf "Bad command-line arguments: ~v\n" v)
        (exit)]))
 
-  ;; Save original current-{input output}-port i.e. stdin/stdout, to
-  ;; give to command-server-loop.
-  (define in  (current-input-port))
-  (define out (current-output-port))
-
-  ;; Set current-{input output}-port to no-ops -- so e.g. some random
-  ;; println won't bork the the command protocol.
-  ;;
-  ;; Do so BEFORE calling any start-xxx functions, below -- so any
-  ;; threads those might create will inherit these no-op values.
-  (current-output-port (open-output-nowhere))
-  (current-input-port  (open-input-bytes #""))
-
-  (start-repl-session-server command-port launch-token)
-
-  (start-logger-server (add1 command-port) launch-token)
-
-  (command-server-loop in out))
+  ;; Save original current-{input output}-port to give to
+  ;; command-server-loop for command I/O.
+  (let ([stdin  (current-input-port)]
+        [stdout (current-output-port)])
+    ;; Set no-ops so e.g. rando print can't bork the command I/O.
+    (parameterize ([current-input-port  (open-input-bytes #"")]
+                   [current-output-port (open-output-nowhere)])
+      (start-repl-session-server command-port launch-token)
+      (start-logger-server (add1 command-port) launch-token)
+      (command-server-loop stdin stdout))))
