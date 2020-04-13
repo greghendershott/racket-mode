1dbb2615669bf147de9d89022f30607080644cfe
Greg Hendershott <git@greghendershott.com>
2020-04-12 23:38:13 -0400

    Set font-lock-multiline for delimiter, too; fixes #443

--- racket-font-lock.el
+++ racket-font-lock.el
@@ -260,27 +260,31 @@ like \"#; #; 1 2\". For more examples see the issue 432 section
 of example/example.rkt."
   (while (re-search-forward (rx "#;") limit t)
     (if (racket--string-or-comment-p (match-beginning 0))
-        (goto-char (match-end 0))       ;issues 388, 408
-      (racket--region-set-face (match-beginning 0) (match-end 0)
-                               'font-lock-comment-delimiter-face t)
-      ;; Font-lock and count any additional successive prefixes
-      (goto-char (match-end 0))
-      (forward-comment (buffer-size))
-      (let ((num-prefixes 1))
-        (while (looking-at (rx "#;"))
-          (cl-incf num-prefixes)
-          (racket--region-set-face (match-beginning 0) (match-end 0)
-                                   'font-lock-comment-delimiter-face t)
-          (goto-char (match-end 0))
-          (forward-comment (buffer-size)))
-        ;; Font-lock as many successive sexprs as prefixes
-        (dotimes (_ num-prefixes)
-          (let ((beg (point)))
-            (forward-sexp 1)
-            (racket--region-set-face beg (point)
-                                     'font-lock-comment-face t)
-            (put-text-property beg (point) 'font-lock-multiline t)
-            (forward-comment (buffer-size)))))))
+        (goto-char (match-end 0))       ;issues #388, #408
+      (let ((first-prefix-begin (match-beginning 0)))
+        (racket--region-set-face (match-beginning 0) (match-end 0)
+                                 'font-lock-comment-delimiter-face t)
+        ;; Font-lock and count any additional successive prefixes
+        (goto-char (match-end 0))
+        (forward-comment (buffer-size))
+        (let ((num-prefixes 1))
+          (while (looking-at (rx "#;"))
+            (cl-incf num-prefixes)
+            (racket--region-set-face (match-beginning 0) (match-end 0)
+                                     'font-lock-comment-delimiter-face t)
+            (goto-char (match-end 0))
+            (forward-comment (buffer-size)))
+          ;; Font-lock as many successive sexprs as prefixes
+          (dotimes (_ num-prefixes)
+            (let ((beg (point)))
+              (forward-sexp 1)
+              (racket--region-set-face beg (point)
+                                       'font-lock-comment-face t)
+              (forward-comment (buffer-size)))))
+        ;; Cover everything from the beginning of the first prefix to
+        ;; the end of the last sexp with font-lock-multiline; #443.
+        (put-text-property first-prefix-begin (point)
+                           'font-lock-multiline t))))
   nil)
 
 (defun racket--string-or-comment-p (pos)
