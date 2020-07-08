ee34deef12febd42283c2c52fbcac99d4f48f08e
Greg Hendershott <git@greghendershott.com>
2020-07-08 15:34:54 -0400

    Fix bugs with special-form indent

--- racket/sexp-indent.rkt
+++ racket/sexp-indent.rkt
@@ -26,7 +26,7 @@
 
 ;; token-map? positive-integer? -> nonnegative-integer?
 (define (indent-amount tm indent-pos)
-  (match (backward-up tm (beg-of-line tm indent-pos))
+  (match (open-pos tm indent-pos)
     [(? number? open-pos)
      (define id-pos (forward-whitespace tm (add1 open-pos)))
      (define id-name (token-text tm id-pos))
@@ -53,22 +53,47 @@
      (log-racket-mode-debug "indent-amount no containing sexp found")
      0]))
 
+(define (open-pos tm pos)
+    (match (classify tm pos)
+      [(bounds+token beg _end (? token:expr:close?))
+       (backward-sexp tm beg)]
+      [_ (backward-up tm (beg-of-line tm pos))]))
+
+;; "The first NUMBER arguments of the function are distinguished
+;; arguments; the rest are considered the body of the expression. A
+;; line in the expression is indented according to whether the first
+;; argument on it is distinguished or not. If the argument is part of
+;; the body, the line is indented lisp-body-indent more columns than
+;; the open-parenthesis starting the containing expression. If the
+;; argument is distinguished and is either the first or second
+;; argument, it is indented twice that many extra columns. If the
+;; argument is distinguished and not the first or second argument, the
+;; line uses the standard pattern."
 (define (special-form tm open-pos id-pos indent-pos special-args)
   ;; Up to special-args get extra indent, the remainder get normal
   ;; indent.
-  (define containing-column (- open-pos (beg-of-line tm open-pos)))
+  (define indent-pos-bol (beg-of-line tm indent-pos))
+  (define (first-arg-on-indent-line? pos)
+    (= (beg-of-line tm pos) indent-pos-bol))
   (define args
     (let loop ([arg-end (forward-sexp tm (forward-sexp tm id-pos))]
                [count 0])
-      (log-racket-mode-debug "special-form ~v" (list (token-text tm (sub1 arg-end)) arg-end id-pos))
-      (if (<= arg-end indent-pos)
+      (if (and arg-end
+               (<= arg-end indent-pos))
           (match (forward-sexp tm arg-end)
+            [(? integer? n) #:when (first-arg-on-indent-line? n) (add1 count)]
             [(? integer? n) (loop n (add1 count))]
             [#f (add1 count)])
           count)))
-  ;;(println (list special-args args))
-  (+ containing-column
-     (if (< args special-args) 4 2)))
+  (define (open-column)
+    (- open-pos (beg-of-line tm open-pos)))
+  (cond [(= args special-args)          ;first non-special
+         (+ (open-column) 2)]
+        [(< special-args args)          ;subsequent non-special
+         (max (+ (open-column) 2)
+              (default-amount tm id-pos))]
+        [else                           ;special
+         (+ (open-column) 4)]))
 
 (define (indent-for tm open-pos id-pos indent-pos)
   ;; TODO
@@ -86,13 +111,13 @@
   ;; TODO
   0)
 
-(define (default-amount tm 1st-sexp)
-  (define bol (beg-of-line tm 1st-sexp))
-  (define eol (end-of-line tm 1st-sexp))
-  (define 2nd-sexp (beg-of-next-sexp tm 1st-sexp))
-  (if (and 2nd-sexp (< 2nd-sexp eol)) ;2nd on same line as 1st?
-      (- 2nd-sexp bol)
-      (- 1st-sexp bol)))
+(define (default-amount tm id-pos)
+  (define bol (beg-of-line tm id-pos))
+  (define eol (end-of-line tm id-pos))
+  (define 1st-arg-pos (beg-of-next-sexp tm id-pos))
+  (if (and 1st-arg-pos (< 1st-arg-pos eol)) ;on same line?
+      (- 1st-arg-pos bol)
+      (- id-pos bol)))
 
 (define (beg-of-next-sexp tm pos)
   (backward-sexp tm
@@ -100,20 +125,18 @@
                                (forward-sexp tm pos))))
 
 #;(require racket/trace)
-#;
-(trace indent-amount
+#;(trace indent-amount
        special-form
        default-amount
        beg-of-next-sexp)
 
 (module+ test
-  (require rackunit
-           racket/pretty)
-  (define str "#lang racket\n(foo\n  bar\nbaz)\n(foo bar baz\nbap)\n(define (f x)\nx)\n(begin0\n42\n1\n2)")
-  ;;           1234567890123 45678 901234 56789 012345678 901234567 89012345678901 234 56789012 345 67 89
-  ;;                    1           2           3          4          5         6           7
-  (define tm (create str))
-  (let* ()
+  (require rackunit)
+  (let ()
+    (define str "#lang racket\n(foo\n  bar\nbaz)\n(foo bar baz\nbap)\n(define (f x)\nx)\n")
+    ;;           1234567890123 45678 901234 56789 012345678 901234567 89012345678901 234 56789012 345 67 89
+    ;;                    1           2           3          4          5         6           7
+    (define tm (create str))
     (check-equal? (indent-amount tm  1) 0
                   "not within any sexpr, should indent 0")
     (check-equal? (indent-amount tm 14) 0
@@ -133,22 +156,55 @@
     (check-equal? (indent-amount tm 43) 5
                   "bap should indent with the 2nd sexp on the same line i.e. bar")
     (check-equal? (indent-amount tm 62) 2
-                  "define body should indent 2")
-    (check-equal? (indent-amount tm 73) 4
+                  "define body should indent 2"))
+  (let ()
+    (define str "#lang racket\n(begin0\n42\n1\n2)\n")
+    ;;           1234567890123 45678901 234 56 789 0
+    ;;                    1          2             3
+    (define tm (create str))
+    (check-equal? (indent-amount tm 22) 4
                   "begin0 result should indent 4")
-    (check-equal? (indent-amount tm 76) 2
-                  "begin0 other should indent 2")
-    (check-equal? (indent-amount tm 78) 2
-                  "begin0 other should indent 2"))
-  (let* ([str "#lang racket\n(print 12"]
-         ;;    1234567890123 4567890123
-         ;;             1           2
-         [tm (create str)])
+    (check-equal? (indent-amount tm 25) 2
+                  "begin0 first other expr should indent 2")
+    (update tm 25 0 "  ") ;actually indent that, for next check...
+    (check-equal? (indent-amount tm 27) 2
+                  "begin0 second other expr should indent same as first other"))
+  (let ()
+    (define str "#lang racket\n(print 12")
+    ;;           1234567890123 4567890123
+    ;;                    1           2
+    (define tm (create str))
     (update tm 23 0 "\n")
     (check-equal? (indent-amount tm  1) 0
                   "not within any sexpr, should indent 0")
     (check-equal? (indent-amount tm 22) 0
-                  "not within any sexpr, should indent 0")))
+                  "not within any sexpr, should indent 0"))
+  (let ()
+    (define str "#lang racket\n(if 123)\n(do)")
+    ;;           1234567890123 456789012 3456
+    ;;                    1          2
+    (define tm (create str))
+    (update tm 21 0 "\n")
+    (check-equal? (indent-amount tm  22) 4
+                  "position is exactly on close token"))
+  (let ()
+    (define str "#lang racket\n(cond [#t #t]\n[#f #f])")
+    ;;           1234567890123 45678901234567 89012345
+    ;;                    1          2          3
+    (check-equal? (indent-amount (create str) 28)
+                  6
+                  "second cond clause indented properly"))
+  (let ()
+    (define str "#lang racket\n(cond\n  [#t #t]\n  [#f #f])")
+    ;;           1234567890123 456789 0123456789 012345
+    ;;                    1           2          3
+    (define tm (create str))
+    (check-equal? (indent-amount tm 22)
+                  2
+                  "first cond clause indented properly")
+    (check-equal? (indent-amount tm 32)
+                  2
+                  "second cond clause indented properly")))
 
 (module+ example-5
   (define str "#lang racket\n123\n(print 123)\n")
@@ -160,6 +216,9 @@
   (update tm 28 0 "\n")
   tm
   (indent-amount tm 29))
+
+(module+ example-6
+  )
 
 ;;; Hardwired macro indents
 
