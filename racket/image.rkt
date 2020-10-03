2625f974ad154947247c28796ad1346917d96d91
Greg Hendershott <git@greghendershott.com>
2020-10-03 10:00:34 -0400

    Use non-bounds convert formats as fall-backs; #475
    When used as a fallback, a default large-ish width is used, so that
    pretty-printing images inside e.g. a list will err on the side of
    printing only one per line, but that seems like the least worst thing
    to do when the width is unknown; #477 just isn't possible.

--- racket/image.rkt
+++ racket/image.rkt
@@ -11,14 +11,18 @@
 
 (define (convert-image v)
   (and (convertible? v)
-       (or (convert-and-save v 'svg-bytes+bounds "svg")
-           (convert-and-save v 'png-bytes+bounds "png"))))
+       (for/or ([fmt/ext (in-list '((svg-bytes+bounds "svg")
+                                    (png-bytes+bounds "png")
+                                    (svg              "svg")
+                                    (png-bytes        "png")))])
+         (apply convert-and-save v fmt/ext))))
 
 (define (convert-and-save v fmt ext)
+  (define (default-width _) 4096)
   (match (convert v fmt)
-    [(list* (? bytes? bstr) width _)
+    [(or (list* (? bytes? bstr) width _)                  ;bytes+bounds
+         (and (? bytes? bstr) (app default-width width))) ;bytes
      (define filename (make-temporary-file (~a "racket-image-~a." ext)))
      (with-output-to-file filename #:exists 'truncate (λ () (display bstr)))
      (cons (format "#<Image: ~a>" filename) width)]
     [_ #f]))
-
