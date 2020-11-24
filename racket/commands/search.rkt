5e9b4f75a0183770e55270bf799737f9b2e79cf4
yilinwei <yilin.wei10@gmail.com>
2020-11-24 14:21:11 +0000

    Filter out results with a score of 6 or greater.

--- racket/commands/search.rkt
+++ racket/commands/search.rkt
@@ -2,6 +2,7 @@
 
 (require
  racket/string
+ racket/function
  racket/list
  racket/format
  racket/contract
@@ -28,14 +29,28 @@
        #:when (not (eq? start end)))
     (substring str start end)))
 
+; Variant of `filter-not` which only removes a single element
+(define (filter-not-1 f lst)
+  (define (loop lst result)
+    (cond
+      [(null? lst)
+       (reverse result)]
+      [(f (car lst))
+       (append
+        (reverse result)
+        (cdr lst))]
+      [else
+       (loop (cdr lst) (cons (car lst) result))]))
+  (loop lst '()))
+
 (define (entry-score entry expr)
   (define words (entry-words entry))
   ; This algorithm is roughly-speaking the same one as
   ; found in `search.js` which is responsible for the search on the manual.
   (cond
     [(equal? expr (car words)) 0]
-    [(string-prefix? expr (car words)) 1]
-    [(string-contains? expr (car words)) 2]
+    [(string-prefix? (car words) expr) 1]
+    [(string-contains? (car words) expr) 2]
     [else
      (let*
          ([expr-sub-words (string->sub-words expr)]
@@ -44,11 +59,18 @@
                         ([sub-word (in-list expr-sub-words)])
                       (member sub-word entry-sub-words))]
           [same-length? (eq? (length expr-sub-words)
-                             (length entry-sub-words))])
+                             (length entry-sub-words))]
+          [complete? (for/fold
+                         ([sub-words entry-sub-words])
+                         ([sub-word (in-list expr-sub-words)])
+                         (filter-not-1
+                          (curry equal? sub-word)
+                          sub-words))])
        (cond
-         [(and matched? same-length?) 3]
-         [matched? 4]
-         [else 5]))]))
+         [(and matched? same-length? complete?) 3]
+         [(and matched? same-length?) 4]
+         [matched? 5]
+         [else 6]))]))
 
 (define from-libs/c
   (or/c symbol?
@@ -83,14 +105,20 @@
         (compose entry->list cdr)
         results))
       ([entry (in-list index)])
-    (define sorted
-      (sort
-       (cons
-        (cons (entry-score entry expr) entry)
-        results)
-       less-than?))
-    (if (> num-results len)
-        (values sorted (add1 len))
+    (define score
+      (entry-score entry expr))
+    (define-values (sorted sorted-len)
+      (if (< score 6)
+          (values
+           (sort
+            (cons
+             (cons score entry)
+             results)
+            less-than?)
+           (add1 len))
+          (values results len)))
+    (if (> num-results sorted-len)
+        (values sorted sorted-len)
         (values (take sorted num-results) len))))
 
 (define (less-than? x y)
@@ -103,5 +131,8 @@
   (check-equal? (string->sub-words "foo-bar") '("foo" "-" "bar"))
   (check-equal? (string->sub-words "foo") '("foo"))
   (check-equal? (string->sub-words "-") '("-"))
+
+  (check-equal? (filter-not-1 (curry eq? 1) '(1 1 2 3 4)) '(1 2 3 4))
+  (check-equal? (filter-not-1 (curry eq? 3) '(1 2 3 4)) '(1 2 4))
 
   (check-true (list? (search "set!" 1))))
