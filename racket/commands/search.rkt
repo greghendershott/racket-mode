4cdb31e936c5856327691ff4214bf767c6f17882
yilinwei <yilin.wei10@gmail.com>
2020-11-24 14:07:44 +0000

    Add search score algorithm

--- racket/commands/search.rkt
+++ racket/commands/search.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
 
 (require
+ racket/string
  racket/list
  racket/format
  racket/contract
@@ -10,11 +11,44 @@
 
 (define index (xref-index xref))
 
+(define (string->sub-words str)
+
+  (define (pair->list pair)
+    (list (car pair) (cdr pair)))
+
+  (define points
+    (append-map
+     pair->list
+     ; Use any punctuation as a delimiter
+     (regexp-match-positions* #px"\\p{P}+" str)))
+
+  (for/list
+      ([start (cons 0 points)]
+       [end (append points (list (string-length str)))]
+       #:when (not (eq? start end)))
+    (substring str start end)))
+
 (define (entry-score entry expr)
   (define words (entry-words entry))
+  ; This algorithm is roughly-speaking the same one as
+  ; found in `search.js` which is responsible for the search on the manual.
   (cond
     [(equal? expr (car words)) 0]
-    [else 10000]))
+    [(string-prefix? expr (car words)) 1]
+    [(string-contains? expr (car words)) 2]
+    [else
+     (let*
+         ([expr-sub-words (string->sub-words expr)]
+          [entry-sub-words (string->sub-words (car words))]
+          [matched? (for/and
+                        ([sub-word (in-list expr-sub-words)])
+                      (member sub-word entry-sub-words))]
+          [same-length? (eq? (length expr-sub-words)
+                             (length entry-sub-words))])
+       (cond
+         [(and matched? same-length?) 3]
+         [matched? 4]
+         [else 5]))]))
 
 (define from-libs/c
   (or/c symbol?
@@ -39,7 +73,7 @@
 
 (define/contract (search expr num-results)
   (-> string? integer?
-      (listof entry/c))
+     (listof entry/c))
   (for/fold
       ([results '()]
        ; Keep the length of the list in memory, to avoid O(n) `length` operation.
@@ -63,3 +97,11 @@
   (< (car x) (car y)))
 
 (provide search)
+
+(module+ test
+  (require rackunit)
+  (check-equal? (string->sub-words "foo-bar") '("foo" "-" "bar"))
+  (check-equal? (string->sub-words "foo") '("foo"))
+  (check-equal? (string->sub-words "-") '("-"))
+
+  (check-true (list? (search "set!" 1))))
