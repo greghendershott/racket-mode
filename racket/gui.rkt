4414c261bd00253af3f2cf4a6599ab3491bfd62b
Greg Hendershott <git@greghendershott.com>
2021-06-13 09:48:44 -0400

    Simplify require-gui
    There is no need to create an eventspace and set current-eventspace to
    it. Just use a boolean flag.
    
    Note: It is tempting to use racket/gui/dynamic here. Unfortunately
    that would mean a package dependency on all of racket-gui-lib, which
    would defeat most of the point of lazily loading the gui
    on-demand. (The remaining, lesser part of the point is to start
    faster, as well as to avoid any default GUI frame window and/or Racket
    application icon that might be created on certain operating systems.)

--- racket/gui.rkt
+++ racket/gui.rkt
@@ -7,28 +7,19 @@
          require-gui
          txt/gui)
 
-(define root-eventspace #f) ;#f until racket/gui/base required first time
+(define done? #f)
 
 (define (gui-required?)
-  (and root-eventspace #t))
+  done?)
 
-;; Extra explanation for situations like issue 93, entering `(require
-;; redex)` in the REPL, as opposed to having it in a .rkt file.
-(define more-explanation
-  @~a{The namespace was reset. Any `require`s you entered in the REPL were "undone".
-      This includes the `require` you just entered. You may want to enter it again.})
-
-;; This must be called from the main thread, under the main custodian!
 (define (require-gui in-repl?)
   (when (gui-required?)
     (error 'require-gui "Already required"))
   (display-commented "On-demand, one-time instantiation of racket/gui/base.")
   (when in-repl?
     (display-commented more-explanation))
-  (define current-eventspace (gui-dyn-req 'current-eventspace))
-  (define make-eventspace    (gui-dyn-req 'make-eventspace))
-  (set! root-eventspace (make-eventspace))
-  (current-eventspace root-eventspace))
+  (set! done? #t)
+  (dynamic-require 'racket/gui/base #f))
 
 ;; Like mz/mr from racket/sandbox.
 (define-syntax txt/gui
@@ -40,3 +31,9 @@
 
 (define (gui-dyn-req sym)
   (dynamic-require 'racket/gui/base sym))
+
+;; Extra explanation for situations like issue 93, entering `(require
+;; redex)` in the REPL, as opposed to having it in a .rkt file.
+(define more-explanation
+  @~a{The namespace was reset. Any `require`s you entered in the REPL were "undone".
+      This includes the `require` you just entered. You may want to enter it again.})
