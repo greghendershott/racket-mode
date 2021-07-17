be42551f5c11dde5b3ea8d2c52642d6d960241da
Greg Hendershott <git@greghendershott.com>
2021-07-17 12:56:27 -0400

    Show/hide initial frame on Windows; closes #301

--- racket/gui.rkt
+++ racket/gui.rkt
@@ -12,6 +12,16 @@
 (with-handlers ([exn:fail? void])
   (dynamic-require 'racket/gui/base #f))
 
+;; #301: On Windows, show then hide an initial frame.
+(when (and (gui-available?)
+           (eq? (system-type) 'windows))
+  (define make-object (dynamic-require 'racket/class 'make-object))
+  (define frame% (dynamic-require 'racket/gui/base 'frame%))
+  (define f (make-object frame% "Emacs Racket Mode initialization" #f 100 100))
+  (define dynamic-send (dynamic-require 'racket/class 'dynamic-send))
+  (dynamic-send f 'show #t)
+  (dynamic-send f 'show #f))
+
 ;; Like mz/mr from racket/sandbox.
 (define-syntax txt/gui
   (syntax-rules ()
