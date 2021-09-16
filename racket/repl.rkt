greghendershott/racket-mode

commit 7f12cb1ff2774e7592632d8aab1572f57d045419

Eagerly load racket/gui/base when available
Try to dynamic-require racket/gui/base when the back end starts. If
that fails just ignore the error on the assumption that it is a
minimal Racket installation. Thereafter use racket/gui/dynamic to
handle both scenarios.

Note that racket/gui/dynamic is supplied by the `base` package ---
this does NOT create a dependency on the `gui-lib` package.

-----------------------------------------------------------------

In contrast the old approach was lazy: Use a module-name-resolver to
catch a user program's first attempt to require racket/gui/base,
require it ourselves, and restart the load of the user program.

One justification for the old approach was to avoid a hard dependency
on gui-lib --- but using dynamic-require and racket/gui/dynamic also
avoids this.

Another justification was to minimize startup time -- but any
difference seems barely measurable to me on semi-modern laptops.

A final justification was that merely requiring racket/gui/base could
cause OS window system artifacts to appear, such as a dummy window or
"Racket" application icon in a task bar or dock --- but I no longer
see this; maybe the behavior of the operating systems and/or Racket
has changed over the years. What I do see now:

Ubuntu 18.04 (Gnome) and Windows 10: No OS artifact appears until
`(send frame show #t)`. When all frames are hidden, the OS window
disappears.

macOS (Catalina): No OS artifact appears until `(send frame show #t)`.
When all frames are hidden, an "exec" icon in the application switcher
remains --- which isn't ideal but also isn't something Racket Mode
can/should handle.

-----------------------------------------------------------------

The background here, for any approach:

1. racket/gui/base cannot be instantiated more than once per Racket
process; attempting to do so causes an error.

2. A GUI program and its REPL need to run under an eventspace thread
via queue-callback, so that GUI events get a chance to be handled.

racket/repl.rkt
@@ -227,29 +227,28 @@
     ;; 3. If module, require and enter its namespace, etc.
     (with-expanded-syntax-caching-evaluator
       (when (and maybe-mod mod-path)
-        (parameterize ([current-module-name-resolver module-name-resolver-for-run])
-          ;; When exn:fail during module load, re-run.
-          (define (load-exn-handler exn)
-            (display-exn exn)
-            (channel-put (current-repl-msg-chan)
-                         (struct-copy run-config cfg [maybe-mod #f]))
-            (sync never-evt)) ;manager thread will shutdown custodian
-          (with-handlers ([exn? load-exn-handler])
-            (with-stack-checkpoint
-              #;(maybe-configure-runtime mod-path) ;FIRST: see #281
-              (namespace-require mod-path)
-              (for ([submod (in-list extra-submods-to-run)])
-                (define submod-spec `(submod ,(build-path dir file) ,@submod))
-                (when (module-declared? submod-spec)
-                  (dynamic-require submod-spec #f)))
-              ;; User's program may have changed current-directory;
-              ;; use parameterize to set for module->namespace but
-              ;; restore user's value for REPL.
-              (current-namespace
-               (parameterize ([current-directory dir])
-                 (module->namespace mod-path)))
-              (maybe-warn-about-submodules mod-path context-level)
-              (check-#%top-interaction))))))
+        ;; When exn:fail during module load, re-run.
+        (define (load-exn-handler exn)
+          (display-exn exn)
+          (channel-put (current-repl-msg-chan)
+                       (struct-copy run-config cfg [maybe-mod #f]))
+          (sync never-evt)) ;manager thread will shutdown custodian
+        (with-handlers ([exn? load-exn-handler])
+          (with-stack-checkpoint
+            #;(maybe-configure-runtime mod-path) ;FIRST: see #281
+            (namespace-require mod-path)
+            (for ([submod (in-list extra-submods-to-run)])
+              (define submod-spec `(submod ,(build-path dir file) ,@submod))
+              (when (module-declared? submod-spec)
+                (dynamic-require submod-spec #f)))
+            ;; User's program may have changed current-directory;
+            ;; use parameterize to set for module->namespace but
+            ;; restore user's value for REPL.
+            (current-namespace
+             (parameterize ([current-directory dir])
+               (module->namespace mod-path)))
+            (maybe-warn-about-submodules mod-path context-level)
+            (check-#%top-interaction)))))
     ;; 4. Update information about our session -- now that
     ;; current-namespace is possibly updated, and, it is OK to
     ;; call get-repl-submit-predicate.
@@ -263,8 +262,7 @@
     ;; send a response.
     (ready-thunk)
     ;; 6. read-eval-print-loop
-    (parameterize ([current-prompt-read (make-prompt-read maybe-mod)]
-                   [current-module-name-resolver module-name-resolver-for-repl])
+    (parameterize ([current-prompt-read (make-prompt-read maybe-mod)])
       ;; Note that read-eval-print-loop catches all non-break
       ;; exceptions.
       (read-eval-print-loop)))
@@ -290,7 +288,7 @@
          [instrumenting-enabled (instrument-level? context-level)]
          [profiling-enabled (eq? context-level 'profile)]
          [test-coverage-enabled (eq? context-level 'coverage)])
-      ;; Run repl-thunk on a plain thread, or, on the eventspace
+      ;; Run repl-thunk on a plain thread, or, on GUI eventspace
       ;; thread via queue-callback. Return the thread.
       (define current-eventspace (txt/gui (make-parameter #f) current-eventspace))
       (parameterize ([current-eventspace ((txt/gui void make-eventspace))])
@@ -320,8 +318,6 @@
        (λ () (sync (current-repl-msg-chan)))))
     (match message
       [(? run-config? c) (clean-up-and-run c)]
-      [(load-gui repl?)  (require-gui repl?)
-                         (clean-up-and-run cfg)]
       [(zero-column ch)  (zero-column!)
                          (channel-put ch 'done)
                          (get-message)]
@@ -380,25 +376,6 @@
   (unless (memq '#%top-interaction (namespace-mapped-symbols))
     (display-commented
      "Because the language used by this module provides no #%top-interaction\n you will be unable to evaluate expressions here in the REPL.")))
-
-;; Catch attempt to load racket/gui/base for the first time.
-(define (make-module-name-resolver repl?)
-  (let ([orig-resolver (current-module-name-resolver)])
-    (define (resolve mp rmp stx load?)
-      (when (and load? (memq mp '(racket/gui/base
-                                  racket/gui/dynamic
-                                  scheme/gui/base)))
-        (unless (gui-required?)
-          (channel-put (current-repl-msg-chan)
-                       (load-gui repl?))
-          (sync never-evt)))
-      (orig-resolver mp rmp stx load?))
-    (case-lambda
-      [(rmp ns)           (orig-resolver rmp ns)]
-      [(mp rmp stx)       (resolve mp rmp stx #t)]
-      [(mp rmp stx load?) (resolve mp rmp stx load?)])))
-(define module-name-resolver-for-run  (make-module-name-resolver #f))
-(define module-name-resolver-for-repl (make-module-name-resolver #t))
 
 ;;; Output handlers; see issues #381 #397
 

racket/gui.rkt
@@ -1,39 +1,21 @@
-#lang at-exp racket/base
+#lang racket/base
 
-(require (only-in racket/format ~a)
-         "util.rkt")
+;; Note that racket/gui/dynamic is in `base` package --- requiring it
+;; does NOT create a dependency on the `gui-lib` package.
+(require racket/gui/dynamic)
 
-(provide gui-required?
-         require-gui
-         txt/gui)
+(provide txt/gui)
 
-(define done? #f)
-
-(define (gui-required?)
-  done?)
-
-(define (require-gui in-repl?)
-  (when (gui-required?)
-    (error 'require-gui "Already required"))
-  (display-commented "On-demand, one-time instantiation of racket/gui/base.")
-  (when in-repl?
-    (display-commented more-explanation))
-  (set! done? #t)
+;; Load racket/gui/base eagerly, if available, instantiating it in our
+;; namespace and under our main custodian (as opposed to those for
+;; user programs).
+(with-handlers ([exn:fail? void])
   (dynamic-require 'racket/gui/base #f))
 
 ;; Like mz/mr from racket/sandbox.
 (define-syntax txt/gui
   (syntax-rules ()
     [(_ txtval guisym)
-     (if (gui-required?)
-         (gui-dyn-req 'guisym)
+     (if (gui-available?)
+         (dynamic-require 'racket/gui/base 'guisym)
          txtval)]))
-
-(define (gui-dyn-req sym)
-  (dynamic-require 'racket/gui/base sym))
-
-;; Extra explanation for situations like issue 93, entering `(require
-;; redex)` in the REPL, as opposed to having it in a .rkt file.
-(define more-explanation
-  @~a{The namespace was reset. Any `require`s you entered in the REPL were "undone".
-      This includes the `require` you just entered. You may want to enter it again.})

commit 4dbf807eb640536467f737b7eaf0029749273789

Set current-get-interaction-port for line/column locations
Ensure that when racket/gui/base sets current-get-interaction-port to
return one that wraps the original, it's using the port to which we
applied port-count-lines!.

Fixes #556 and fixes #519 again (latter was regressed by commit
7f12cb1).

racket/repl.rkt
@@ -167,7 +167,11 @@
             (log-racket-mode-fatal "Authorization failed: ~v"
                                    supplied-token)
             (exit 'racket-mode-repl-auth-failure))
-          (port-count-lines! in)  ;#519
+          ;; port-count-lines! for things like #519, #556. Furthermore
+          ;; set current-get-interaction-port to return this so that
+          ;; racket/gui/base's override respects it.
+          (port-count-lines! in)
+          (current-get-interaction-input-port (λ () in))
           (port-count-lines! out) ;for fresh-line
           (thread repl-manager-thread-thunk))))
     (accept-a-connection)))