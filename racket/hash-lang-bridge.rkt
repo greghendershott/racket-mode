51a84f28e68747c64db3f52e26051174f78f7435
Greg Hendershott <git@greghendershott.com>
2021-11-14 22:21:02 -0500

    Rename and clean up
    Rename some things from "lexindent" and "token-map" to hash-lang.
    
    Delete many files that are now N/A compared to the approach from a
    year ago, including steps toward making some of the code its own
    "token-map" package.
    
    For now, just focus on this being code for Racket Mode. Although I
    want to contribute the efficient interval-map idea for use in
    expeditor and other tools, some of that is still a moving target, and
    anyway I want to continue validating/refining things for use in Emacs.
    When my piece settles down, as well as some things in expeditor, I can
    take another look at how best to make some of this available for
    reuse.
    
    Also note:
    
    - This needs Racket 6.12+ for interval-map-ref/bounds.
    
    - A few tests need `framework` and are skipped on headless Racket --
    e.g. CI like GitHub Actions -- but do run locally.

--- racket/hash-lang-bridge.rkt
+++ racket/hash-lang-bridge.rkt
@@ -5,10 +5,10 @@
 (require racket/async-channel
          racket/class
          racket/match
-         "../token-map/private/core.rkt"
-         "../util.rkt")
+         "hash-lang.rkt"
+         "util.rkt")
 
-(provide lexindent
+(provide hash-lang
          token-notify-channel)
 
 ;; TODO:
@@ -18,7 +18,7 @@
 ;; - Default to module-lexer* not module-lexer?? IIUC the main difference
 ;;   is that token type can be a hash-table instead of a symbol.
 
-(define (lexindent . args)
+(define (hash-lang . args)
   (log-racket-mode-debug "~v" args)
   (match args
     [`(create ,id ,s)                             (create id s)]
@@ -30,9 +30,9 @@
 
 (define token-notify-channel (make-async-channel))
 
-(struct lexindenter (obj notify-rx-chan) #:transparent)
+(struct obj+chan (obj notify-rx-chan) #:transparent)
 (define ht (make-hash)) ;id => lexindenter?
-(define (get-object id) (lexindenter-obj (hash-ref ht id)))
+(define (get-object id) (obj+chan-obj (hash-ref ht id)))
 
 (define (create id s) ;any/c string? -> void
   ;; We supply an async-channel to create that we receive here to
@@ -56,11 +56,11 @@
          ['quit (void)]))))
   (define obj (new hash-lang% [notify-chan ch]))
   (send obj update! 1 1 0 s)
-  (hash-set! ht id (lexindenter obj ch)))
+  (hash-set! ht id (obj+chan obj ch)))
 
 (define (delete id)
   (match (hash-ref ht id #f)
-    [(lexindenter obj ch)
+    [(obj+chan obj ch)
      (send obj delete)
      (async-channel-put ch 'quit) ;kill thread
      (hash-remove! ht id)]
@@ -85,49 +85,49 @@
 (module+ example-0
   (define id 0)
   (define str "#lang racket\n42 (print \"hello\") @print{Hello} 'foo #:bar")
-  (lexindent 'create id str)
-  (lexindent 'update id 2 14 2 "9999")
-  (lexindent 'classify id 2 14)
-  (lexindent 'update id 3 14 4 "")
-  (lexindent 'classify id 3 14)
-  (lexindent 'classify id 3 15)
-  (lexindent 'grouping id 3 15 'forward 0 1))
+  (hash-lang 'create id str)
+  (hash-lang 'update id 2 14 2 "9999")
+  (hash-lang 'classify id 2 14)
+  (hash-lang 'update id 3 14 4 "")
+  (hash-lang 'classify id 3 14)
+  (hash-lang 'classify id 3 15)
+  (hash-lang 'grouping id 3 15 'forward 0 1))
 
 (module+ example-1
   (define id 0)
   (define str "#lang at-exp racket\n42 (print \"hello\") @print{Hello (there)} 'foo #:bar")
-  (lexindent 'create id str)
-  (lexindent 'classify id 1 (sub1 (string-length str))))
+  (hash-lang 'create id str)
+  (hash-lang 'classify id 1 (sub1 (string-length str))))
 
 (module+ example-1.5
   (define id 0)
   (define str "#lang scribble/manual\n(print \"hello\")\n@print[#:kw 12]{Hello (there) #:not-a-keyword}\n")
-  (lexindent 'create id str))
+  (hash-lang 'create id str))
 
 (module+ example-2
   (define id 0)
   (define str "#lang scribble/text\nHello @(print \"hello\") @print{Hello (there)} #:not-a-keyword")
-  (lexindent 'create id str)
-  (lexindent 'classify id (sub1 (string-length str))))
+  (hash-lang 'create id str)
+  (hash-lang 'classify id (sub1 (string-length str))))
 
 (module+ example-3
   (define id 0)
   (define str "#lang racket\n(λ () #t)")
-  (lexindent 'create id str)
-  (lexindent 'classify id 1 14)
-  (lexindent 'classify id 1 (sub1 (string-length str))))
+  (hash-lang 'create id str)
+  (hash-lang 'classify id 1 14)
+  (hash-lang 'classify id 1 (sub1 (string-length str))))
 
 (module+ example-4
   (define id 0)
   (define str "#lang racket\n#rx\"1234\"\n#(1 2 3)\n#'(1 2 3)")
-  (lexindent 'create id str))
+  (hash-lang 'create id str))
 
 (module+ example-5
   (define id 0)
   (define str "#lang racket\n123\n(print 123)\n")
   ;;           1234567890123 4567 890123456789 0
   ;;                    1           2          3
-  (lexindent 'create id str)
+  (hash-lang 'create id str)
   (indent-amount id 1 18)
   (update id 2 28 0 "\n")
   (indent-amount id 2 29))
